c60baa97d0fdaae9140b707e17abebc7ef52fbb0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-29 16:57:06 +0200

    add field signaltype to Morlet1DSpec

--- src/morlet1d.jl
+++ src/morlet1d.jl
@@ -1,8 +1,9 @@
-    ɛ
 immutable Morlet1DSpec{T<:Number} <: AbstractSpec1D{T}
+    ɛ # ::realtype(T) (imposed by inner constructor)
+    signaltype::Type{T}
     log2_length::Int
-    max_qualityfactor
-    max_scale
+    max_qualityfactor # ::realtype(T) (imposed by inner constructor)
+    max_scale # ::realtype(T) (imposed by inner constructor)
     nFilters_per_octave::Int
     nOctaves::Int
     function Morlet1DSpec{T<:Number}(ɛ, log2_length, max_qualityfactor,
