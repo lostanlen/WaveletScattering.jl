7e7e1e9c787c0fda9e12959eb17eb0b25b7a6d76
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-29 10:07:35 +0200

    s/bankgammas/testgammas/g

--- src/spec.jl
+++ src/spec.jl
@@ -2,7 +2,7 @@ abstract AbstractSpec
 abstract AbstractSpec1D{T<:Number} <: AbstractSpec
 abstract AbstractSpec2D{T<:Number} <: AbstractSpec
 
-function bankgammas(spec::AbstractSpec)
+function specgammas(spec::AbstractSpec)
     nΓs = spec.nFilters_per_octave * spec.nOctaves
     γs = collect(0:(nΓs-1))
     chromas = collect(0:(spec.nFilters_per_octave-1))
--- test/spec.jl
+++ test/spec.jl
@@ -6,27 +6,27 @@ end
 # abstract subtype testing
 @test TestSpec1D <: AbstractSpec
 
-# bankgammas
+# specgammas
 spec = TestSpec1D(1, 1)
-(γs, χs, js) = bankgammas(spec)
+(γs, χs, js) = specgammas(spec)
 @test γs == [0]
 @test χs == [0]
 @test js == [0]
 
 spec = TestSpec1D(2, 1)
-(γs, χs, js) = bankgammas(spec)
+(γs, χs, js) = specgammas(spec)
 @test γs == [0 1]
 @test χs == [0 1]
 @test js == [0 0]
 
 spec = TestSpec1D(1, 2)
-(γs, χs, js) = bankgammas(spec)
+(γs, χs, js) = specgammas(spec)
 @test γs == [0 1]
 @test χs == [0 0]
 @test js == [0 1]
 
 spec = TestSpec(12, 8)
-(γs, χs, js) = bankgammas(spec)
+(γs, χs, js) = specgammas(spec)
 nWavelets = spec.nFilters_per_octave * spec.nOctaves
 @test length(γs) == nWavelets
 @test length(χs) == nWavelets
