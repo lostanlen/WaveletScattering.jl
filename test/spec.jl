1bc60f978228acddb8a5af6d06ec11980b1b78ed
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-29 17:51:09 +0200

    TestSpec*D -> Test*DSpec

--- test/spec.jl
+++ test/spec.jl
@@ -1,40 +1,40 @@
-import WaveletScattering: AbstractSpec, AbstractSpec1D, AbstractSpec2D,
+import WaveletScattering: AbstractSpec, Abstract1DSpec, Abstract2DSpec,
                           specgammas
 
-immutable TestSpec1D <: AbstractSpec1D
+immutable Test1DSpec <: Abstract1DSpec
     nFilters_per_octave::Int
     nOctaves::Int
 end
-immutable TestSpec2D <: AbstractSpec2D
+immutable Test2DSpec <: Abstract2DSpec
     nFilters_per_octave::Int
     nOctaves::Int
     nOrientations::Int
 end
 
 # abstract subtype testing
-@test TestSpec1D <: AbstractSpec
-@test TestSpec2D <: AbstractSpec
+@test Test1DSpec <: AbstractSpec
+@test Test2DSpec <: AbstractSpec
 
 # specgammas
-spec = TestSpec1D(1, 1)
+spec = Test1DSpec(1, 1)
 (γs, χs, js) = specgammas(spec)
 @test γs == [0]
 @test χs == [0]
 @test js == [0]
 
-spec = TestSpec1D(2, 1)
+spec = Test1DSpec(2, 1)
 (γs, χs, js) = specgammas(spec)
 @test γs == [0, 1]
 @test χs == [0, 1]
 @test js == [0, 0]
 
-spec = TestSpec1D(1, 2)
+spec = Test1DSpec(1, 2)
 (γs, χs, js) = specgammas(spec)
 @test γs == [0, 1]
 @test χs == [0, 0]
 @test js == [0, 1]
 
-spec = TestSpec1D(12, 8)
+spec = Test1DSpec(12, 8)
 (γs, χs, js) = specgammas(spec)
 nWavelets = spec.nFilters_per_octave * spec.nOctaves
 @test length(γs) == nWavelets
