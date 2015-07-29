5e039d1ecab2acbdee2ba3be85f3e9cc8591c574
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-29 09:54:19 +0200

    write bankgammas(spec::AbstractSpec)

--- src/spec.jl
+++ src/spec.jl
@@ -1,3 +1,13 @@
 abstract AbstractSpec
 abstract AbstractSpec1D{T<:Number} <: AbstractSpec
 abstract AbstractSpec2D{T<:Number} <: AbstractSpec
+
+function bankgammas(spec::AbstractSpec)
+    nΓs = spec.nFilters_per_octave * spec.nOctaves
+    γs = collect(0:(nΓs-1))
+    chromas = collect(0:(spec.nFilters_per_octave-1))
+    χs = repmat(chromas, spec.nOctaves)
+    octaves = collect(0:(spec.nOctaves-1))
+    js = vec(repmat(transpose(octaves), spec.nFilters_per_octave))
+    return (γs, χs, js)
+end
