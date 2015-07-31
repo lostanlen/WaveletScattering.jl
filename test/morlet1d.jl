9d5eacacb26184715a76b7e15027d8d332a53700
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-31 11:55:50 +0200

    fix test of Morlet1DSpec constructor

--- test/morlet1d.jl
+++ test/morlet1d.jl
@@ -1,16 +1,16 @@
 import WaveletScattering: Morlet1DSpec, realtype
 
 # Morlet1DSpec constructor
-for signaltype in [Float16, Float32, Float64,
+for T in [Float16, Float32, Float64,
                    Complex{Float16}, Complex{Float32}, Complex{Float64}]
-    RealT = realtype(signaltype)
+    RealT = realtype(T)
     ɛ = 1e-5
     log2_length = 15
     max_qualityfactor = 8.0
     max_scale = 10000.0
     nFilters_per_octave = 12
     nOctaves = 8
-    m = Morlet1DSpec(ɛ, signaltype, log2_length, max_qualityfactor, max_scale,
+    m = Morlet1DSpec{T}(ɛ, T, log2_length, max_qualityfactor, max_scale,
         nFilters_per_octave, nOctaves)
     @test isa(m.ɛ, RealT)
     @test m.signaltype == signaltype
