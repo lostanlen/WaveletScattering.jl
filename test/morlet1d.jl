5a59691949ea598acf485d2737ae098c71516e24
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-31 14:33:56 +0200

    bugfix test/morlet1d.jl spec constructor

--- test/morlet1d.jl
+++ test/morlet1d.jl
@@ -9,10 +9,10 @@ for T in [Float16, Float32, Float64, Complex{Float16}, Complex{Float32}, Complex
     max_scale = 10000.0
     nFilters_per_octave = 12
     nOctaves = 8
-    m = Morlet1DSpec{T}(ɛ, T, log2_length, max_qualityfactor, max_scale,
+    spec = Morlet1DSpec{T}(ɛ, T, log2_length, max_qualityfactor, max_scale,
         nFilters_per_octave, nOctaves)
-    @test isa(m.ɛ, RealT)
-    @test m.signaltype == T
-    @test isa(max_qualityfactor, RealT)
-    @test isa(max_scale, RealT)
+    @test isa(spec.ɛ, RealT)
+    @test spec.signaltype == T
+    @test isa(spec.max_qualityfactor, RealT)
+    @test isa(spec.max_scale, RealT)
 end
