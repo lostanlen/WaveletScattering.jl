1423bc160ed174101cc3daee5b01ca613da9deb4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-31 14:48:07 +0200

    write checkspec(::Options) method

--- src/spec.jl
+++ src/spec.jl
@@ -3,6 +3,18 @@ abstract AbstractSpec
 abstract Abstract1DSpec{T<:Number} <: AbstractSpec
 abstract Abstract2DSpec{T<:Number} <: AbstractSpec
 
+function checkspec(opts::Options)
+    if opts[:max_qualityfactor]<1.0
+        error("max_qualityfactor cannot be lower than 1.0.")
+    end
+    if opts[:max_qualityfactor]>opts[:nFilters_per_octave]
+        error("max_qualityfactor cannot be greater than nFilters_per_octave.")
+    end
+end
+
+realtype{T<:Real}(::Type{T}) = T
+realtype{T<:Real}(::Type{Complex{T}}) = T
+
 function specgammas(spec::AbstractSpec)
     nΓs = spec.nFilters_per_octave * spec.nOctaves
     γs = collect(0:(nΓs-1))
@@ -12,6 +24,3 @@ function specgammas(spec::AbstractSpec)
     js = vec(repmat(transpose(octaves), spec.nFilters_per_octave))
     return (γs, χs, js)
 end
-
-realtype{T<:Real}(::Type{T}) = T
-realtype{T<:Real}(::Type{Complex{T}}) = T
