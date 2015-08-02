c724d12783e4a68dfabcd81153630d2539bf7076
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-02 09:38:51 +0200

    test Morlet1DSpec default options

--- test/morlet1d.jl
+++ test/morlet1d.jl
@@ -1,7 +1,10 @@
 import WaveletScattering: Morlet1DSpec, realtype
 
+numerictypes = [Float16, Float32, Float64,
+    Complex{Float16}, Complex{Float32}, Complex{Float64}]
+
 # Morlet1DSpec constructor
-for T in [Float16, Float32, Float64, Complex{Float16}, Complex{Float32}, Complex{Float64}]
+for T in numerictypes
     RealT = realtype(T)
     ɛ = 1e-5
     log2_length = 15
@@ -15,4 +18,23 @@ for T in [Float16, Float32, Float64, Complex{Float16}, Complex{Float32}, Complex
     @test spec.signaltype == T
     @test isa(spec.max_qualityfactor, RealT)
     @test isa(spec.max_scale, RealT)
+end
+
+# Morlet1DSpec default options
+for T in numerictypes
+  opts = @options
+  RealT = realtype(T)
+  spec = Morlet1DSpec(opts)
+  @test spec.ɛ == eps(RealT)
+  @test spec.log2_length = 15
+  @test_approx_eq spec.max_qualityfactor == 1.0
+  @test_approx_eq spec.max_scale == Inf
+  @test spec.max_nFilters_per_octave == 1
+  @test spec.nOctaves == 8
+  opts = @options max_qualityfactor = 8.0
+  spec = Morlet1DSpec(opts)
+  @test spec.max_nFilters_per_octave == 8
+  spec = Morlet1DSpec(opts)
+  opts = @options nFilters_per_octave = 12
+  @test_approx_eq spec.max_qualityfactor == 12.0
 end
