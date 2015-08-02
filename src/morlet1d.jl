866589daa5e06405bc86772fb1e44ae0d457a8ae
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-02 13:54:06 +0200

    update checkspec callsite

--- src/morlet1d.jl
+++ src/morlet1d.jl
@@ -10,6 +10,7 @@ immutable Morlet1DSpec{T<:Number} <: Abstract1DSpec{T}
                                      max_qualityfactor, max_scale,
                                      nFilters_per_octave::Int, nOctaves::Int)
         RealT = realtype(T)
+        checkspec(max_qualityfactor, nFilters_per_octave)
         new(RealT(ɛ), T, log2_length, RealT(max_qualityfactor),
             RealT(max_scale), nFilters_per_octave, nOctaves)
     end
@@ -17,21 +18,20 @@ end
 
 function Morlet1DSpec(opts::Options{CheckError})
     @defaults opts signaltype = Float32
-    T = opts[:signaltype]
+    T = signaltype
     RealT = realtype(T)
     @defaults opts ɛ = eps(RealT)
     @defaults opts log2_length = 15
     @defaults opts max_qualityfactor = one(RealT)
-    if haskey(opts.key2index, :nFilters_per_octave)
-        @defaults opts max_qualityfactor = float(opts[:nFilters_per_octave])
-    else
+    if opts[:nFilters_per_octave] == nothing
         @defaults opts max_qualityfactor = one(RealT)
+    else
+        @defaults opts max_qualityfactor = float(nFilters_per_octave)
     end
     @defaults opts max_scale = RealT(Inf)
     @defaults opts nFilters_per_octave = ceil(max_qualityfactor)
     @defaults opts nOctaves = 8
     @check_used opts
-    checkspec(opts)
     Morlet1DSpec{T}(ɛ, signaltype, log2_length, max_qualityfactor, max_scale,
                  nFilters_per_octave, nOctaves)
 end
