0cfab1f0edadd161eff14681910f3f46d9dedf5e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-03 23:38:03 +0200

    remove deprecated require("Options")

--- src/WaveletScattering.jl
+++ src/WaveletScattering.jl
@@ -1,15 +1,11 @@
 module WaveletScattering
 
-require("Options")
-
 using Compat
 using DataStructures
 using OptionsMod
 
-
 include("spec.jl")
 include("morlet1d.jl")
-
 include("variables.jl")
 
 end
