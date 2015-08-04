09b97953f81009aa6877b7058f83aa7a06a28c98
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-04 09:40:24 +0200

    update test/morlet1d.jl

--- test/morlet1d.jl
+++ test/morlet1d.jl
@@ -5,16 +5,9 @@ numerictypes = [Float16, Float32, Float64,
 
 # Morlet1DSpec constructor
 for T in numerictypes
-    RealT = realtype(T)
-    ɛ = 1e-5
-    log2_length = 15
-    max_qualityfactor = 8.0
-    max_scale = 10000.0
-    nFilters_per_octave = 12
-    nOctaves = 8
-    spec = Morlet1DSpec{T}(ɛ, log2_length, max_qualityfactor, max_scale,
-        nFilters_per_octave, nOctaves, T)
-    @test isa(spec.ɛ, RealT)
+    spec = Morlet1DSpec{T}(T, ɛ=1e-5, log2_length=15, max_qualityfactor=8.0,
+        max_scale=1e4, nFilters_per_octave=12, nOctaves=8)
+    @test isa(spec.ɛ, realtype(T))
     @test spec.signaltype == T
     @test isa(spec.max_qualityfactor, Float64)
     @test isa(spec.max_scale, Float64)
@@ -24,7 +17,7 @@ end
 for T in numerictypes
   RealT = realtype(T)
   # ordinary defaults, user-specified nOctaves
-  spec = Morlet1DSpec(@options signaltype=T nOctaves=8)
+  spec = Morlet1DSpec(signaltype=T, nOctaves=8)
   @test spec.ɛ == eps(RealT)
   @test spec.log2_length == 15
   @test_approx_eq spec.max_qualityfactor 1.0
@@ -32,11 +25,11 @@ for T in numerictypes
   @test spec.nFilters_per_octave == 1
   @test spec.nOctaves == 8
   # nFilters_per_octave defaults to max_qualityfactor when it is provided
-  spec = Morlet1DSpec(@options max_qualityfactor=8.0)
+  spec = Morlet1DSpec(max_qualityfactor=8.0)
   @test spec.nFilters_per_octave == 8
   @test spec.nOctaves == 10
   # max_qualityfactor defaults to nFilters_per_octave when it is provided
-  spec = Morlet1DSpec(@options nFilters_per_octave=12)
+  spec = Morlet1DSpec(nFilters_per_octave=12)
   @test_approx_eq spec.max_qualityfactor 12.0
   @test spec.nOctaves == 9
 end
@@ -47,12 +40,12 @@ spec = Morlet1DSpec()
 
 # localize
 # in the dyadic case, check that the mother center center frequency is 0.39
-spec = Morlet1DSpec(@options nFilters_per_octave=1)
+spec = Morlet1DSpec(nFilters_per_octave=1)
 (bandwidths, centerfrequencies, qualityfactors, scales) = localize(spec)
 @test_approx_eq centerfrequencies[1] 0.39
 nfos = [2, 4, 8, 12, 16, 24, 32]
 for nfo in nfos[2:end]
-    spec = Morlet1DSpec(@options nFilters_per_octave=nfo)
+    spec = Morlet1DSpec(nFilters_per_octave=nfo)
     # check that the mother center frequency is at the right place
     (bandwidths, centerfreqs, qualityfactors, scales) = localize(spec)
     @test_approx_eq (centerfreqs[1]-centerfreqs[2]) (1.0 - 2*centerfreqs[1])
@@ -65,12 +58,8 @@ end
 for RealT in [Float16, Float32, Float64], nfo in nfos,
   max_q in 1:nfo, max_s in [exp2(11:16); Inf]
     machine_precision = max(1e-10, eps(RealT))
-    opts = @options begin
-      signaltype=RealT
-      max_qualityfactor=max_q
-      max_scale=max_s
-      nFilters_per_octave=nfo end
-    spec = Morlet1DSpec(opts)
+    spec = Morlet1DSpec(max_qualityfactor=max_q, max_scale=max_s,
+        nFilters_per_octave=nfo, signaltype=RealT)
     (bandwidths, centerfrequencies, qualityfactors, scales) = localize(spec)
     ten_epsilon = 10.0*eps(RealT)
     @test all(qualityfactors.>=1.0)
