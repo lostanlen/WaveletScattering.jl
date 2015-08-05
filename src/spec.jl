e5f4cfe9733cf4bd427a66ed909359d33a41b5a3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-05 14:39:47 +0200

    split specgammas + better doc

--- src/spec.jl
+++ src/spec.jl
@@ -1,4 +1,4 @@
-"""An AbstractSpec object contains all the immutable specifications in a
+"""An `AbstractSpec` object contains all the immutable specifications in a
 wavelet filter bank"""
 abstract AbstractSpec
 abstract Abstract1DSpec{T<:Number} <: AbstractSpec
@@ -7,7 +7,7 @@ abstract Abstract2DSpec{T<:Number} <: AbstractSpec
 """Enforces properties of the wavelets to satisfy null mean, limited spatial
 support, and Littlewood-Paley inequality.
 
-* The truncation threshold ɛ must be in [0.0, 1.0[.
+* The truncation threshold `ɛ`` must be in [0.0, 1.0[.
 
 * The signal length must be a finite power of two above 4.
 
@@ -16,11 +16,10 @@ filters per octaves.
 
 * The maximum scale must be at least 5 times above the maximum quality factor.
 
-* The lowest center frequency must be higher than 1, i.e. log2_length>nOctaves.
+* The lowest center frequency must be above 1.0, i.e. `log2_length>nOctaves`.
 
-* The lowest center frequency must be higher than half the number of
-filters per octaves, i.e. (log2_length-nOctaves) >= log2(nFilters_per_octave).
-"""
+* The lowest center frequency must be higher than half the number of filters
+per octaves, i.e. `(log2_length-nOctaves) >= log2(nFilters_per_octave)`."""
 function checkspec(ɛ, log2_length, max_qualityfactor, max_scale,
                   nFilters_per_octave, nOctaves)
     if ɛ>=one(ɛ) || ɛ<zero(ɛ)
@@ -78,57 +77,75 @@ function checkspec(ɛ, log2_length, max_qualityfactor, max_scale,
     return true
 end
 
-"""Given an signal input type T, returns a conservative default value for
-the truncation threshold ɛ of the wavelets.
+"""Returns the chroma indices `χs` (locations within the octave).
+Chroma indices range from `0` to `nFilters_per_octave-1`. The convention is
+that higher chroma indices `χs` mean *lower* center frequencies. Log-periods
+`γs`, chromas ``χs`, and octaves `js` are linked by
+    γ = j + nFilters_per_octave * χ"""
+function chromas(spec::AbstractSpec)
+    repmat(collect(0:(spec.nFilters_per_octave-1)), spec.nOctaves)
+end
+
+"""Given an signal input type `T`, returns a conservative default value for
+the truncation threshold `ɛ of the wavelets.
 The chosen values are powers of ten within the same order of magnitude as
-floating-point machine precision eps(T)."""
+floating-point machine precision `eps(T)`."""
 default_ɛ(T::Type{Float16}) = 1e-3
 default_ɛ(T::Type{Float32}) = 1e-7
 default_ɛ(T::Type{Float64}) = 1e-15
 default_ɛ{RealT}(T::Type{Complex{RealT}}) = default_ɛ(RealT)
 
 """Given a maximum quality factor and a number of filter per octaves (both of
-which may be Void), returns the maximum quality factor in a wavelet filter
+which may be `Void`), returns the maximum quality factor in a wavelet filter
 bank."""
 default_max_qualityfactor(max_q::Float64, nfo::Any) = max_q
 default_max_qualityfactor(max_q::Void, nfo::Integer) = Float64(nfo)
 default_max_qualityfactor(max_q::Void, nfo::Void) = 1.0
 
 """Given a maximum quality factor and a number of filter per octaves (both of
-which may be Void), returns the default number of filters per octave in a
+which may be `Void`), returns the default number of filters per octave in a
 wavelet filter bank."""
 default_nFilters_per_octave(max_q::Any, nfo::Int) = nfo
 default_nFilters_per_octave(max_q::Float64, nfo::Void) = ceil(Int, max_q)
 default_nFilters_per_octave(max_q::Void, nfo::Void) = 1
 
-"Generic fallback for default_nOctaves when nOctaves is not a Void input."
+"Generic fallback for `default_nOctaves when `nOctaves` is not a `Void` input."
 default_nOctaves{T<:AbstractSpec}(::Type{T}, nOctaves::Int, args...) = nOctaves
 
+"""Returns the wavelet log-period integer indices `γs`. Center frequencies are
+proportional to 2^(-γ). γ ranges from 0 to nFilters_per_octave*nOctaves, where
+γ=0 corresponds to the mother frequency. The convention is that higher indices
+`γs` mean *lower* center frequencies. Log-periods γs`, chromas ``χs`, and
+octaves `js` are linked by
+    γ = j + nFilters_per_octave * χ"""
+function gammas(spec::AbstractSpec)
+    collect(0:(spec.nFilters_per_octave * spec.nOctaves-1))
+end
+
 """Returns the type parameter of a complex type.
-For example, realtype(Complex{Float32}) returns Float32.
-For numeric real types, e.g. Float32, it is a no-op."""
+For example, `realtype(Complex{Float32})` returns `Float32`.
+For numeric real types, e.g. `Float32`, it is a no-op."""
 realtype{T<:Real}(::Type{T}) = T
 realtype{T<:Real}(::Type{Complex{T}}) = T
 
-"""Returns a tuple of log-scales `γs`, chromas `χs`, and octaves `js` in a
-given spec. The input spec must have fields nFilters_per_octave and nOctaves."""
-function specgammas(spec::AbstractSpec)
-    nΓs = spec.nFilters_per_octave * spec.nOctaves
-    γs = collect(0:(nΓs-1))
-    chromas = collect(0:(spec.nFilters_per_octave-1))
-    χs = repmat(chromas, spec.nOctaves)
-    octaves = collect(0:(spec.nOctaves-1))
-    js = vec(repmat(transpose(octaves), spec.nFilters_per_octave))
-    return (γs, χs, js)
+"""Returns the octave indices js of a wavelet spec.
+Octave indices range from `0` to `nOctaves-1`. The convention is that
+higher octave indices `js` mean *lower* center frequencies. Log-periods
+``γs`, chromas ``χs`, and octaves `js` are linked by
+    γ = j + nFilters_per_octave * χ"""
+function octaves(spec::AbstractSpec)
+    vec(repmat(transpose(collect(0:(spec.nOctaves-1))), spec.nFilters_per_octave))
 end
 
 """Given a dimensionless tuning frequency, returns the maximal admissible
 mother frequency such that the subsequent wavelets will be in tune with the
 tuning frequency.
 
-```ξ = tune(Morlet1DSpec, 12, 440.0/44100.0)
-Morlet1DSpec(nFilters_per_octave=12, mother_frequency=ξ)``
-tunes a 12-chroma filter bank to a concert pitch of 440Hz."""
+For example, to tune a 12-chroma filter bank to a concert pitch of 440 Hz at
+a sample rate of 44,1 kHz:
+
+    ξ = tune(Morlet1DSpec, 12, 440.0/44100.0)
+    Morlet1DSpec(nFilters_per_octave=12, mother_frequency=ξ)"""
 function tune{T<:AbstractSpec}(::Type{T}, nFilters_per_octave, tuning_frequency)
     max_centerfrequency = default_motherfrequency(T, nFilters_per_octave)
     tuning_ratio = max_centerfrequency / tuning_frequency
