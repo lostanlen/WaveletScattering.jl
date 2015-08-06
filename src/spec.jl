42b74b98aa31b983ececa094facb935c43b8ca8b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-06 10:43:15 +0200

    floor(Int, log2(motherfrequency / min_centerfrequency))

--- src/spec.jl
+++ src/spec.jl
@@ -164,7 +164,7 @@ function default_nOctaves{T}(nOctaves::Void, ::Type{T}, log2_size::Tuple,
     else
         min_centerfrequency = uncertainty(T) / max_scale * 1.0
     end
-    return floor(log2(motherfrequency / min_centerfrequency))
+    return floor(Int, log2(motherfrequency / min_centerfrequency))
 end
 
 """Returns the wavelet log-period integer indices `γs`. Center frequencies are
