9a3b48539f544f01265cb4e5c8637749c122af83
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-06 13:20:10 +0200

    remove checkspec tests

--- test/spec.jl
+++ test/spec.jl
@@ -4,67 +4,6 @@ import WaveletScattering: AbstractSpec, Abstract1DSpec, Abstract2DSpec,
     tune_motherfrequency
 import WaveletScattering: Morlet1DSpec
 
-# checkspec
-ɛ = eps(Float32)
-log2_size = (15,)
-max_qualityfactor = 12.0
-max_scale = 1e5
-motherfrequency = 0.48
-nFilters_per_octave = 12
-nOctaves = 8
-# normal behavior
-@test
-    checkspec(ɛ, log2_size, max_qualityfactor,
-              max_scale, motherfrequency, nFilters_per_octave, nOctaves)
-# error if ɛ < 0.0
-@test_throws ErrorException
-    checkspec(-0.1, log2_size, max_qualityfactor,
-              max_scale, motherfrequency, nFilters_per_octave, nOctaves)
-# error if ɛ >= 1.0
-@test_throws ErrorException
-    checkspec(1.0, log2_size, max_qualityfactor,
-              max_scale, motherfrequency, nFilters_per_octave, nOctaves)
-# error if log2_size < 2
-@test_throws ErrorException
-    checkspec(ɛ, 1, max_qualityfactor,
-              max_scale, motherfrequency, nFilters_per_octave, nOctaves)
-# error if max_qualityfactor < 1.0
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, 0.9,
-              max_scale, motherfrequency, nFilters_per_octave, nOctaves)
-# error if motherfrequency <= 0
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, max_qualityfactor,
-              max_scale, 0.0, nFilters_per_octave, nOctaves)
-# error if motherfrequency > 0.5
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, max_qualityfactor,
-              max_scale, 0.51, nFilters_per_octave, nOctaves)
-# error if nFilters_per_octave < 1
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, max_qualityfactor,
-              max_scale, motherfrequency, 0, nOctaves)
-# error if nOctaves < 1
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, max_qualityfactor,
-              max_scale, motherfrequency, nFilters_per_octave, 0)
-# error if nOctave >= log2_size
-@test_throws ErrorException
-    checkspec(ɛ, 9, max_qualityfactor,
-              max_scale, motherfrequency, 1, 9)
-# error if log2_size-nOctaves <= log2(nFilters_per_octave)
-@test_throws ErrorException
-    checkspec(ɛ, 12, max_qualityfactor,
-              max_scale, motherfrequency, 32, 9)
-# error if max_qualityfactor > nFilters_per_octave
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, 8.0,
-              max_scale, motherfrequency, 4, nOctaves)
-# error if max_scale < (5.0*max_qualityfactor)
-@test_throws ErrorException
-    checkspec(ɛ, log2_size, 32.0,
-              100.0, motherfrequency, nFilters_per_octave, nOctaves)
-
 # default_ɛ
 @test_approx_eq default_ɛ(Float16) 1e-3
 @test_approx_eq default_ɛ(Float32) 1e-7
@@ -79,8 +18,8 @@ nOctaves = 8
 @test_approx_eq default_max_qualityfactor(nothing, nothing) 1.0
 
 # default_nFilters_per_octave
-@test default_nFilters_per_octave(nothing, 12) == 12.0
-@test default_nFilters_per_octave(7.9, nothing) == 8
+@test default_nFilters_per_octave(12, nothing) == 12
+@test default_nFilters_per_octave(nothing, 7.9) == 8
 @test default_nFilters_per_octave(nothing, nothing) == 1
 
 # realtype
