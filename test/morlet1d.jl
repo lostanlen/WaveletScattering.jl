23aabca01a2a705db8bf44117f81cd90d7fa6b4d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-12 15:37:40 +0200

    move tests from morlet1d.jl to spec.jl

--- test/morlet1d.jl
+++ test/morlet1d.jl
@@ -33,39 +33,10 @@ spec = Morlet1DSpec()
 nfos = [1, 2, 4, 8, 12, 16, 24, 32]
 for nfo in nfos
     spec = Morlet1DSpec(nFilters_per_octave=nfo)
-    # 1. check that the mother center frequency is at the right place
     ξs = centerfrequencies(spec)
     if nfo==1
         @test_approx_eq ξs[1] 0.39
     else
         @test_approx_eq (ξs[1]-ξs[2]) (1.0 - 2*ξs[1])
     end
-    # 2. check that log-frequencies are evenly spaced
-    difflogξs = diff(log2(ξs))
-    @test_approx_eq difflogξs (-ones(difflogξs)/spec.nFilters_per_octave)
-    # 3. check that all center frequencies are strictly positive
-    @test all(ξs.>0.0)
-end
-# qualityfactors, scales, bandwidths, and time-frequency tradeoff.
-for T in [Float16, Float32, Float64], nfo in nfos,
-  max_q in 1:nfo, max_s in [exp2(7:16); Inf]
-  @show T, nfo, max_q, max_s
-    try
-        spec = Morlet1DSpec(T, max_qualityfactor=max_q, max_scale=max_s,
-                            nFilters_per_octave=nfo)
-    catch
-        continue
-    end
-    machine_precision = max(1e-10, default_ɛ(T))
-    bws = bandwidths(spec)
-    ξs = centerfrequencies(spec)
-    qs = qualityfactors(spec)
-    scs = scales(spec)
-    @test all(qs.>=1.0)
-    @test all(qs.<=max_q)
-    @test all(scs.>0.0)
-    @test all(scs[qs.>1.0] .< (max_s+machine_precision))
-    @test all(scs .< (exp2(spec.log2_size[1])+machine_precision))
-    empirical_uncertainty = bws .* scs
-    @test all(abs(diff(empirical_uncertainty)) .< 1e-6)
 end
--- test/spec.jl
+++ test/spec.jl
@@ -5,14 +5,47 @@ import WaveletScattering: AbstractSpec, Abstract1DSpec, Abstract2DSpec,
     gammas, realtype, octaves, qualityfactors, scales, tune_motherfrequency
 import WaveletScattering: Morlet1DSpec
 
-# bandwidths
+# bandwidths, centerfrequencies, default_nOctaves, qualityfactors, scales
 numerictypes = [Float16, Float32, Float64]
 nfos = [1, 2, 4, 8, 12, 24, 32]
 for T in numerictypes, nfo in nfos, max_q in nfos[nfos.<=nfo],
     log2_s in (4+ceil(Int, log2(nfo)):18), max_s in [max_q*exp2(4:14); Inf]
-    spec = Morlet1DSpec(T, nFilters_per_octave=nfo, max_qualityfactor=max_q,
+    machine_precision = max(1e-10, default_ɛ(T))
+    spec = Morlet1DSpec(T, nFilters_per_octave=nfo, max_qualityfactor=max_q
                         log2_size=log2_s, max_scale=max_s)
-
+    bws = bandwidths(spec)
+    ξs = centerfrequencies(spec)
+    qs = qualityfactors(spec)
+    scs = scales(spec)
+    # bandwidths
+    @test_approx_eq bws ξs./qs
+    # centerfrequencies
+    @test_approx_eq ξs[1] spec.mother_frequency
+    difflogξs = diff(log2(ξs))
+    @test_approx_eq difflogξs (-ones(difflogξs)/spec.nFilters_per_octave)
+    @test all(ξs.>0.0)
+    @test_approx_eq ξs bws.*qs
+    # default_nOctaves
+    siglength = 1 << log2_s
+    min_centerfrequency = min(ξs)
+    nOctaves = default_nOctaves(nOctaves, Morlet1DSpec, log2_s, max_q, max_s,
+        spec.motherfrequency, nfo)
+    nOctaves_a = floor(Int, log2(ξs[1] / min(ξs)))
+    nOctaves_b = log2_s - 1 - ceil(Int, log2(nfo))
+    @test nOctaves = min(nOctaves_a, nOctaves_b)
+    # qualityfactors
+    qs = qualityfactors(spec)
+    @test all(qs.>=0.0)
+    @test all(qs.<=max_q)
+    @test_approx_eq qs ξs./bws
+    # scales
+    @test all(scs.>0.0)
+    @test all(scs[qs.>1.0] .< (max_s+machine_precision))
+    @test all(scs .< (exp2(spec.log2_size[1])+machine_precision))
+    # uncertainty
+    empirical_uncertainty = bws .* scs
+    @test all(abs(diff(empirical_uncertainty)) .< 1e-6)
+    @test_approx_eq all(abs(uncertainty(spec)-empirical_uncertainty).<1e-6)
 end
 
 # default_ɛ
@@ -32,6 +65,10 @@ end
 @test default_nFilters_per_octave(12, nothing) == 12
 @test default_nFilters_per_octave(nothing, 7.9) == 8
 @test default_nFilters_per_octave(nothing, nothing) == 1
+
+# default_nOctaves (fallback)
+type WhateverType end
+@test default_nOctaves(5, WhateverType) = 5
 
 # realtype
 @test realtype(Float32) == Float32
@@ -66,11 +103,10 @@ nWavelets = spec.nFilters_per_octave * spec.nOctaves
 # tune_motherfrequency
 nfos = [1, 2, 4, 8, 12, 16, 24]
 pitchforks = [392, 415, 422, 430, 435, 440, 442, 444, 466]
-spectypes = [Morlet1DSpec]
-for nfo in nfos, pitchfork in pitchforks, spectype in spectypes
+for nfo in nfos, pitchfork in pitchforks
     tuning_frequency = pitchfork / 44100.0
     ξ = tune_motherfrequency(spectype, nfo, tuning_frequency)
-    spec = spectype(nFilters_per_octave=nfo, motherfrequency=ξ)
+    spec = Test1DSpec(nFilters_per_octave=nfo)
     γs = gammas(spec)
     ωs = ξ * 2^(-γs / spec.nFilters_per_octave)
     @test any(abs(ωs - ξ) < 1e-4)
