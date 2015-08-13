0d7e2d2ebdfdd6cbc917aa07934213c0a090b765
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 23:37:24 +0200

    more tests for AbstractFourier1DFilter

--- test/fourierfilter.jl
+++ test/fourierfilter.jl
@@ -1,7 +1,7 @@
 using Base.Test
 import WaveletScattering: AbstractFourier1DFilter, Vanishing1DFilter
 
-log2_length = 3
+log2_length = 4
 first = -2
 last = 3
 y = 1 .<< collect(0:(last-first))
@@ -13,6 +13,11 @@ y = 1 .<< collect(0:(last-first))
 @test ψ.an.posfirst == 1
 
 first = -2
-last = 6
+last = 9
 y = 1 .<< collect(0:(last-first))
 ψ = AbstractFourier1DFilter(y, first, last, log2_length)
+@test isa(ψ, VanishingWithMidpoint1DFilter)
+@test ψ.coan.neg == [2048, 0, 0, 0, 0, 1, 2]
+@test ψ.coan.neglast == -1
+@test ψ.an.pos == [8, 16, 32, 64, 128, 256, 512]
+@test ψ.an.posfirst == 1
