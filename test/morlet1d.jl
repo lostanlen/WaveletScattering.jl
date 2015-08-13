89e790c74901a334c81f626043d9d5d2ee701476
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 10:40:39 +0200

    test gauss function

--- test/morlet1d.jl
+++ test/morlet1d.jl
@@ -30,6 +30,7 @@ spec = Morlet1DSpec()
 @test spec.signaltype == Float32
 @test spec.nOctaves == spec.log2_size[1] - 3
 
+# default_motherfrequency
 nfos = [1, 2, 4, 8, 12, 16, 24, 32]
 for nfo in nfos
     spec = Morlet1DSpec(nFilters_per_octave=nfo)
@@ -38,5 +39,16 @@ for nfo in nfos
         @test_approx_eq ξs[1] 0.39
     else
         @test_approx_eq (ξs[1]-ξs[2]) (1.0 - 2*ξs[1])
+    end
+end
+
+# gauss
+@test_approx_eq gauss(1.0, 0.0) == 1.0
+for ω in 1.0:10.0
+    for den in logspace(0, 3, 4)
+        g = gauss(ω, den)
+        @test g >= 0.0
+        @test_approx_eq g gauss(-ω, den)
+        @test_approx_eq sqrt(-log(g) * den) ω
     end
 end
