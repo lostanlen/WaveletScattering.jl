fffe827190f4dff3b3efee688694aeca8859294d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 12:43:31 +0200

    document Behavior

--- src/bank.jl
+++ src/bank.jl
@@ -11,8 +11,20 @@ immutable FourierNonOriented1DBank{T<:Number} <: AbstractNonOrientedBank{T}
     spec::Abstract1DSpec{T}
 end
 
+"""A `Behavior` object contains the mutable information in a filter bank. The
+values of these fields may be changed *after* the construction of the filter
+bank, without having to recompute the underlying architecture. Fields:
 
-# Behavior
+* γ_range: range of wavelet indices that are actually used in the convolutions.
+Default is all of them (see outer constructor).
+
+* log2_oversampling: base-2 logarithm of the oversampling factor with respect
+to the critical sampling rate 2^(-j+1). Must be positive.
+Default is 0, i.e. no oversampling.
+
+* min_log2_resolution : base-2 logarithm of the minimal undersampling factor.
+Default is (-J-1), i.e. it has no effect. Set min_log2_resolution to 0 to avoid
+any undersampling."""
 type Behavior
     γ_range::UnitRange
     log2_oversampling::Int
