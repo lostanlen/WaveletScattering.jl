82289ce84c743da9f355721f03679a93c22c4ec0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 12:29:43 +0200

    concrete subtypes of AbstractFourier1DFilter{T}

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -1,2 +1,23 @@
 abstract AbstractFourierFilter{T<:Number}
 abstract AbstractFourier1DFilter{T<:Number} <: AbstractFourierFilter
+
+immutable Analytic1D{T<:Number}  <: AbstractFourier1DFilter{T}
+    pos::Vector{T}
+    posfirst::Int
+end
+
+immutable Coanalytic1D{T<:Number} <: AbstractFourier1DFilter{T}
+    neg::Vector{T}
+    neglast::Int
+end
+
+immutable Symmetric1D{T<:Number} <: AbstractFourier1DFilter{T}
+    leg::Vector{T}
+    zero::T
+end
+
+immutable Vanishing1D{T<:Number} <: AbstractFourier1DFilter{T}
+    an::Analytic1D{T}
+    coan::Coanalytic1D{T}
+    midpoint::T
+end
