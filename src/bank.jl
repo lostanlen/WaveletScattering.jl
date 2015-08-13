28bbf47b83f8e2e209dc11b508592f7b2aac197a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 12:30:20 +0200

    immutable FourierNonOriented1DBank{T<:Number}

--- src/bank.jl
+++ src/bank.jl
@@ -2,3 +2,12 @@
 abstract AbstractBank{T<:Number}
 abstract AbstractNonOrientedBank{T<:Number} <: AbstractBank{T<:Number}
 abstract AbstractOrientedBank{T<:Number} <: AbstractBank{T<:Number}
+
+immutable FourierNonOriented1DBank{T<:Number} <: AbstractNonOrientedBank{T}
+    ψs::Vector{AbstractFourier1DFilter{T}}
+    ϕs::FourierSymmetric1DFilter{T}
+    behavior::Behavior
+    metas::Vector{NonOrientedMeta}
+    spec::Abstract1DSpec{T}
+end
+
