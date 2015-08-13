3eb4d025e89bae21b2164f668e8901d9ca0c304c
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 21:58:29 +0200

    write AbstractFourier1DFilter (phew!) bis

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -56,3 +56,59 @@ function AbstractFourier1DFilter(y, first, last, N)
             neg = vcat(
                 y[(2-first+N/2):N],
                 y[(1+N):(N-first)] + y[1:(-first)])
+            neglast = -1
+            coan = Coanalytic1DFilter(neg, neglast)
+            pos = vcat(
+                y[(1-first):(1+last-first-N)] + y[(1+N-first):(-first+3N/2)],
+                y[(2+last-first-N):(-first+N/2)])
+            posfirst = 1
+            an = Analytic1DFilter(pos, posfirst)
+            if last == 3N/2
+                midpoint = y[1-first+N/2] + y[1-first+3N/2]
+            else
+                midpoint = y[1-first+N/2]
+            end
+            return VanishingWithMidpoint1DFilter(an, coan, midpoint)
+        end
+    elseif first>0 && first<(N/2)
+        if last<0 && last<(N/2)
+            # support is in ]0;N/2[
+            # we just define y as the analytic part
+            pos = y
+            posfirst = first
+            return Analytic1DFilter(pos, posfirst)
+        elseif last>(N/2+1) && last<N
+            # support is in ]0;N[
+            # we split y between analytic and coanalytic parts
+            # the midpoint N/2 is handled separately
+            pos = y[1:(N/2-first)]
+            posfirst = first
+            an = Analytic1DFilter(pos, posfirst)
+            neg = y[(2+N/2-first):end]
+            neglast = last - N
+            coan = Coanalytic1DFilter(neg, neglast)
+            midpoint = y[1+N/2-first]
+            return VanishingWithMidpoint1DFilter(an, coan, midpoint)
+        elseif last>N && last<(3N/2+1)
+            # support is in ]0;3N/2]
+            # we sum ]0;N/2[ (or a subset) with ]N;N/2[ (or a subset)
+            # we define ]N/2;N[ as the coanalytic part
+            # the midpoint N/2 is handled separately
+            pos = vcat(
+                y[(2+N-first):N],
+                y[1:(N/2-first)]+y[(1+N):(3N/2-first)],
+                y[3N/2-first+1:end])
+            posfirst = 1
+            an = Analytic1DFilter(pos, posfirst)
+            neg = y[(2+N/2-first):(N-first)]
+            neglast = -1
+            coan = Coanalytic1DFilter(neg, neglast)
+            if last == 3N/2
+                midpoint = y[1+N/2-first] + y[1+3N/2-first]
+            else
+                midpoint = y[1+N/2-first]
+            end
+            return VanishingWithMidpoint1DFilter(an, coan, midpoint)
+        end
+    end
+end
