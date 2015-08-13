13fef4b22b3ade66f973f6270e0b3f8179328062
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 22:31:40 +0200

    bugfix test/fourierfilter.jl

--- test/fourierfilter.jl
+++ test/fourierfilter.jl
@@ -2,10 +2,12 @@ using Base.Test
 import WaveletScattering: AbstractFourier1DFilter, Vanishing1DFilter
 
 N = 8
-first = -1
+first = -2
 last = 3
 y = 1 .<< collect(0:(last-first))
 ψ = AbstractFourier1DFilter(y, first, last, N)
 @test isa(ψ, Vanishing1DFilter)
-@test ψ.an == [1]
-@test ψ.coan == [4,8,16]
+@test ψ.coan.neg == [1, 2]
+@test ψ.coan.neglast == -2
+@test ψ.an.pos == [8, 16, 32]
+@test ψ.an.posfirst == 1
