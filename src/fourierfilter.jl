984a954fa9e032674e9203a43ef9d3225cfe4335
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 21:57:50 +0200

    write function AbstractFourier1DFilter (phew!)

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -21,3 +21,38 @@ immutable VanishingWithMidpoint1DFilter{T<:Number} <: AbstractFourier1DFilter{T}
     coan::Coanalytic1DFilter{T}
     midpoint::T
 end
+
+function AbstractFourier1DFilter(y, first, last, N)
+    if first>(-N/2) && first<0
+        if last>0 && last<(N/2)
+            # support is in ]-N/2;N/2[
+            # we split y between analytic and coanalytic parts
+            neg = y[1:(-first)]
+            neglast = first
+            coan = Coanalytic1DFilter(neg, neglast)
+            pos = y[(2-first):end]
+            posfirst = 1
+            an = Analytic1DFilter(pos, posfirst)
+            return Vanishing1DFilter(an, coan)
+        elseif last>(N/2-1) && last<N
+            # support is in ]-N/2;N[
+            # we sum ]-N/2;0[ (or a subset) with ]N/2;N[ (or a subset)
+            # we define ]0;N/2[ as the analytic part
+            # the midpoint N/2 is handled separately
+            neg = vcat(
+                y[(2-first+N/2):N],
+                y[(1+N):end] + y[1:(1+last-first-N)],
+                y[(2+last-first-N):(-first)])
+            neglast = -1
+            coan = Coanalytic1DFilter(neg, neglast)
+            pos = y[(2-first):(-first+N/2)]
+            posfirst = 1
+            an = Analytic1DFilter(pos, posfirst)
+        elseif last>N && last<(3N/2+1)
+            # support is in ]-N/2;3N/2]
+            # we sum ]-N/2;0[ (or a subset) with ]N/2;N[
+            # we sum ]N/2;N[ with ]N;3N/2[ (or a subset)
+            # we sum midpoints N/2 and 3N/2 (if present)
+            neg = vcat(
+                y[(2-first+N/2):N],
+                y[(1+N):(N-first)] + y[1:(-first)])
