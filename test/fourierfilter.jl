76ddb8e2802a10a23ff983f580243014a1873c62
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 22:20:44 +0200

    start testing AbstractFourier1DFilter

---
+++ test/fourierfilter.jl
@@ -0,0 +1,10 @@
+using Base.Test
+
+N = 8
+first = -1
+last = 3
+y = 1 .<< collect(0:(last-first))
+ψ = AbstractFourier1DFilter(y, first, last, N)
+@test isa(ψ, Vanishing1DFilter)
+@test ψ.an = [1]
+@test ψ.coan = [4,8,16]
