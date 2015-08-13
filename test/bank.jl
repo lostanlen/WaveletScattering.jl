97044ed9f6b9203e2f8145a78490383dc30bf8cf
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-13 13:14:22 +0200

    fix test/bank.jl bis

--- test/bank.jl
+++ test/bank.jl
@@ -4,6 +4,6 @@ import WaveletScattering: Behavior
 # Behavior
 js = Int8[0,0,-1,-1,-2,-2,-3,-3]
 b = Behavior(js)
-@test b.γ_range == [0,7]
+@test b.γ_range == 0:7
 @test b.log2_oversampling == 0
 @test b.min_log2_resolution == -2
