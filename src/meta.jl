b16865eb64da208ab44b0a41ea3036ea8cd8811d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-18 19:50:43 +0200

    Declare type OrientedMeta

--- src/meta.jl
+++ src/meta.jl
@@ -5,7 +5,6 @@ abstract AbstractMeta
 """A `NonOrientedMeta` object contains all the meta-information to identify a
 non-oriented wavelet within a filter bank. Fields:
 * γ log-scale. 2^(-γ) is proportional to center frequency
-* θ orientation. θ is proportional to angle (2d) or spin (1d)
 * χ chroma. χ is mod(γ, nFilters_per_octave)
 * j octave. j is div(γ, nFilters_per_octave)
 * bandwidth ∈]0,1] is the width at -3dB, expressed in fraction of signal length
@@ -14,6 +13,27 @@ non-oriented wavelet within a filter bank. Fields:
 * scale is the FWTM (full width at tenth maximum) in spatial domain"""
 immutable NonOrientedMeta <: AbstractMeta
     γ::Int16
+    χ::Int8
+    bandwidth::Float64
+    centerfrequency::Float64
+    j::Int8
+    qualityfactor::Float64
+    scale::Float64
+end
+
+"""An `OrientedMeta` object contains all the meta-information to identify
+an oriented wavelet within a filter bank. Fields:
+* γ log-scale. 2^(-γ) is proportional to center frequency
+* θ orientation, i.e. angle (in 2d) or sign of center frequency (in 1d)
+* χ chroma. χ is mod(γ, nFilters_per_octave)
+* j octave. j is div(γ, nFilters_per_octave)
+* bandwidth ∈]0,1] is the width at -3dB, expressed in fraction of signal length
+* centerfrequency ∈]0,1] is expressed in fraction of signal length
+* qualityfactor ∈[1,max_qualityfactor] is equal to centerfrequency/bandwidth
+* scale is the FWTM (full width at tenth maximum) in spatial domain"""
+immutable OrientedMeta <: AbstractMeta
+    γ::Int16
+    θ::Int8
     χ::Int8
     bandwidth::Float64
     centerfrequency::Float64
