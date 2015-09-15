a3bd1e364edd312d6792151ae0c973a6800e6057
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-15 16:04:32 +0200

    bugfix Base.getindex{T}(ψ::Vanishing1DFilter{T}, I::UnitRange{Int64})

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -126,8 +126,8 @@ function Base.getindex{T}(ψ::Vanishing1DFilter{T}, i::Integer)
 end
 function Base.getindex{T}(ψ::Vanishing1DFilter{T}, I::UnitRange{Int64})
     return [
-        ψ.coan[min(-1, I.start):min(-1, I.stop)];
-        ψ.an[max(0, I.start):max(0, I.stop)] ]
+        ψ.coan[min(-1, I.start):min(max(-1, I.start), I.stop)];
+        ψ.an[max(min(0, I.stop), I.start):max(0, I.stop)] ]
 end
 function Base.getindex(ψ::VanishingWithMidpoint1DFilter, i::Integer)
     halfN = ψ.an.posfirst + length(ψ.an.pos)
@@ -209,7 +209,7 @@ function renormalize!{F<:AbstractFourier1DFilter}(ψs::Vector{F},
     if !isinf(spec.max_scale) && spec.max_qualityfactor>1.0
         ξleft = uncertainty(spec) / spec.max_scale
         ξright = spec.max_qualityfactor * ξleft
-        λs = find([metas[λ].centerfrequency < ξelbow for λ in eachindex(ψs)])
+        λs = find([metas[λ].centerfrequency < ξright for λ in eachindex(ψs)])
         ωs = round(Int, N * ξleft):round(Int, N * ξright)
         ψmat = []
 
