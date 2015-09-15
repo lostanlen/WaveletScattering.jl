2ae2d9cf2c38c225d43b51f4270578585b806afd
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-15 12:03:00 +0200

    bugfix Base.getindex(ψ::VanishingWithMidpoint1DFilter, I::UnitRange{Int64})

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -127,8 +127,8 @@ end
 function Base.getindex(ψ::VanishingWithMidpoint1DFilter, I::UnitRange{Int64})
     halfN = ψ.an.posfirst + length(ψ.pos)
     output = [
-        ψ.coan[halfN + (min(0, I.start):min(0, I.stop))];
-        ψ.coan[halfN + (max(0, I.start):max(0, min(I.stop, halfN-1))]
+        ψ.coan[min(0, I.start):min(0, I.stop)] ;
+        ψ.coan[max(0, I.start):max(0, min(I.stop, halfN-1)] ]
     return (I.stop==halfN ? [ψ.midpoint; output] : output)
 end
 
