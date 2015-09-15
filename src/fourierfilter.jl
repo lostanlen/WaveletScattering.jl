34dfc1ef46edf114e17623860ae8aaa2a88d52d8
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-15 15:31:59 +0200

    bugfix function Base.getindex(ψ::VanishingWithMidpoint1DFilter, I::UnitRange{Int64}) bis

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -139,7 +139,8 @@ function Base.getindex(ψ::VanishingWithMidpoint1DFilter, I::UnitRange{Int64})
     output = [
         ψ.coan[min(-1, I.start, -halfN+1):min(-1, I.stop)] ;
         ψ.an[max(0, I.start):max(0, I.stop)] ]
-    return (I.start==-halfN ? [output ; ψ.midpoint] : output)
+    I.start<=-halfN && output[I.start-halfN+1] = ψ.midpoint
+    return output
 end
 
 """Adds the squared magnitude of a Fourier-domain wavelet `ψ` to an accumulator
