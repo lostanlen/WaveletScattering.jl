31254182318cdc404ecd626e768f68a8362f9a79
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-18 22:47:03 -0400

    add missing closed paren

--- src/morlet1d.jl
+++ src/morlet1d.jl
@@ -109,7 +109,7 @@ end
 
 function scalingfunction{T<:Number}(spec::Morlet1DSpec{T})
     bw = (1<<(spec.log2_size[1]-nOctaves)) *
-         T(uncertainty(spec) spec.motherfrequency
+         T(uncertainty(spec) * spec.motherfrequency)
     den = @fastmath bw * bw / T(2.0 * log(2.0))
     lastω = bw * sqrt(2.0 / spec.ɛ)
     leg = T[ gauss(ω, den) for ω in 1:lastω ]
