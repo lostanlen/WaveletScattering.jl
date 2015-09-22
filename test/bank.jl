f5b7fe257cbc1ea374968ab50da641322119eed6
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-21 22:28:44 -0400

    start testing test FourierNonOriented1DBank

--- test/bank.jl
+++ test/bank.jl
@@ -1,5 +1,9 @@
 using Base.Test
-import WaveletScattering: Behavior
+# bank.jl
+import WaveletScattering: Behavior, FourierNonOriented1DBank,
+    FourierOriented1DBank
+# morlet1d.jl
+import WaveletScattering: Morlet1DSpec
 
 # Behavior
 js = Int8[0,0,1,1,2,2,3,3]
@@ -7,3 +11,7 @@ b = Behavior(js)
 @test b.γ_range == 0:7
 @test b.log2_oversampling == 0
 @test b.min_log2_resolution == -2
+
+# FourierNonOriented1DBank
+spec = Morlet1DSpec()
+bank = FourierNonOriented1DBank(spec)
