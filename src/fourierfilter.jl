ebdb9e196aa8a3fba742fb0d45d1b8ed14e9ae60
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-21 22:55:21 -0400

    bugfix elbowλ in renormalize!

--- src/fourierfilter.jl
+++ src/fourierfilter.jl
@@ -211,7 +211,10 @@ function renormalize!{F<:AbstractFourier1DFilter}(ψs::VecOrMat{F},
     N = 1 << spec.log2_size[1]
     T = spec.signaltype
     if !isinf(spec.max_scale) && spec.max_qualityfactor>1.0
-        elbowλ = 1; while (metas[elbowλ].scale<spec.max_scale) elbowλ += 1; end
+        elbowλ = 1;
+        while (metas[elbowλ].scale<spec.max_scale) && elbowλ<length(metas)
+            elbowλ += 1
+        end
         elbowω = round(Int, N * metas[elbowλ].centerfrequency)
         λs = elbowλ:length(metas)
         ψmat = zeros(T, (elbowω, length(λs)))
