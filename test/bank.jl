173a90085cc6571ebc328ad37ee20418d321253d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-26 10:32:03 +0200

    test audio filter bank

--- test/bank.jl
+++ test/bank.jl
@@ -25,3 +25,8 @@ bank = FourierOriented1DBank(Morlet1DSpec())
     Morlet1DSpec(Float64))
 @test_throws ErrorException FourierOriented1DBank{Float64}(
     Morlet1DSpec(Float32))
+
+#
+spec = Morlet1DSpec(nFilters_per_octave = 24, max_scale = 4410,
+    nOctaves = 8, log2_size = 16)
+bank = FourierNonOriented1DBank(spec)
