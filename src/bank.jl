712ff94af2b296f0db05ca7cc38d20d209299413
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-28 09:38:57 +0200

    revert isreal methods

--- src/bank.jl
+++ src/bank.jl
@@ -109,5 +109,9 @@ immutable FourierOriented1DBank{T<:Number} <: AbstractOrientedBank{T}
         new{T}(ψs, ϕ, behavior, metas, spec)
     end
 end
-FourierOriented1DBank(spec::Abstract1DSpec) =
+function call(::Type{FourierOriented1DBank}, spec::Abstract1DSpec)
+    isreal(spec) && error("Wavelets specified by ", typeof(spec),
+        "are real in the spatial domain. To fix, this error, define a",
+        "FourierNonOriented1DBank instead of a FourierOriented1DBank.")
     FourierOriented1DBank{spec.signaltype}(spec)
+end
