12156891d63482bf08b1824339ae96988e1e33c7
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-04 22:29:13 +0200

    remove obsolete multi-core tests

--- test/bank.jl
+++ test/bank.jl
@@ -36,25 +36,3 @@ bank = FourierOriented1DBank(spec)
 @test typeof(bank.ψs) ==
     Array{AbstractFourier1DFilter{Float32},2}
 @test typeof(bank.ϕ) == Symmetric1DFilter{Float32}
-
-# multi-core mode
-addprocs(1)
-@everywhere import WaveletScattering: Morlet1DSpec
-@everywhere import WaveletScattering: FourierNonOriented1DBank,
-    FourierOriented1DBank
-# FourierNonOriented1DBank
-spec = Morlet1DSpec(nFilters_per_octave = 24, max_scale = 4410,
-    nOctaves = 8, log2_size = 16)
-bank = FourierOriented1DBank(spec)
-@test size(bank.metas) == size(bank.ψs)
-@test typeof(bank.ψs) ==
-    Array{AbstractFourier1DFilter{Float32},2}
-@test typeof(bank.ϕ) == Symmetric1DFilter{Float32}
-spec = Morlet1DSpec(nFilters_per_octave = 24, max_scale = 4410,
-    nOctaves = 8, log2_size = 16)
-bank = FourierNonOriented1DBank(spec)
-@test length(bank.metas) == length(bank.ψs)
-@test typeof(bank.ψs) ==
-    Array{WaveletScattering.AbstractFourier1DFilter{Float32},1}
-@test typeof(bank.ϕ) == Symmetric1DFilter{Float32}
-rmprocs(2)
