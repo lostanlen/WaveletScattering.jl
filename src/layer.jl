ca98b5d2666be352879961fb7cc6af58724f1221
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-06 18:35:36 +0200

    define AbstractNode{T, N}

--- src/layer.jl
+++ src/layer.jl
@@ -26,4 +26,8 @@ immutable ScatteredBlob{T<:Number, N} <: Mocha.Blob{T, N}
     nodes::Dict{Path, AbstractNode{T, N}}
     subscripts::NTuple{PathKey}
 end
+
+# Node
+abstract AbstractNode{T, N}
+
 end
