8e82d964fbfcc1ca34ddb69f1efefd4a28670594
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-06 16:52:47 +0200

    PathKey is no longer a type alias

--- src/path.jl
+++ src/path.jl
@@ -8,9 +8,11 @@ Literal(tup::SymbolInt) = Literal(tup[1], tup[2])
 Literal(sym::Symbol) = Literal(sym, 1)
 
 "A `PathKey` is a linked list of `Literal`s"
-typealias PathKey LinkedList{Literal}
-PathKey() = nil(Literal)
-PathKey(head, tail...) = cons(Literal(head), PathKey(tail...))
+type PathKey
+    list::LinkedList{Literal}
+    PathKey() = new(nil(Literal))
+    PathKey(args...) = new(map(Literal, list(args...)))
+end
 
 """A `Path` is a dictionary whose keys `PathKeys` and whose values are
 integer indices"""
