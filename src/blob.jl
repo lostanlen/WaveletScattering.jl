d87d2e49a067d25e642abb81ee25a8cb0e56c016
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-06 21:53:45 +0200

    bugifx NTuple in ScatteredBlob

--- src/blob.jl
+++ src/blob.jl
@@ -1,7 +1,7 @@
 # ScatteredBlob
 immutable ScatteredBlob{T<:Number, N} <: Mocha.Blob{T, N}
     nodes::Dict{Path, AbstractNode{T, N}}
-    subscripts::NTuple{PathKey}
+    subscripts::NTuple{N, PathKey}
 end
 
 function fft!(blob::ScatteredBlob, dims)
