81f54b485dcbfa616a7baa5314bda6255db1b3eb
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-06 15:46:45 +0200

    indentation

--- src/layer.jl
+++ src/layer.jl
@@ -1,9 +1,9 @@
 # We adopt the same whitespace convention as in the Mocha code base
 Mocha.@defstruct ScatteringLayer Mocha.Layer (
-  name :: AbstractString = "scattering",
-  (bottoms :: Vector{Symbol} = Symbol[], length(bottoms) > 0),
-  (tops :: Vector{Symbol} = Symbol[], length(tops) == length(bottoms)),
-  neuron :: ActivationFunction = Neurons.Identity(),
+    name :: AbstractString = "scattering",
+    (bottoms :: Vector{Symbol} = Symbol[], length(bottoms) > 0),
+    (tops :: Vector{Symbol} = Symbol[], length(tops) == length(bottoms)),
+    neuron :: ActivationFunction = Neurons.Identity(),
 )
 
 Mocha.@characterize_layer(ScatteringLayer,
