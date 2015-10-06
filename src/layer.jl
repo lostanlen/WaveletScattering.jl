0a30b09a605c0b66dd33c54992cbfaa8ab7e47da
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-06 16:00:14 +0200

    rename ScatteringLayer -> WaveletScattering

--- src/layer.jl
+++ src/layer.jl
@@ -1,12 +1,13 @@
+# WaveletLayer
 # We adopt the same whitespace convention as in the Mocha code base
-Mocha.@defstruct ScatteringLayer Mocha.Layer (
+Mocha.@defstruct WaveletLayer Mocha.Layer (
     name :: AbstractString = "scattering",
     (bottoms :: Vector{Symbol} = Symbol[], length(bottoms) > 0),
     (tops :: Vector{Symbol} = Symbol[], length(tops) == length(bottoms)),
-    neuron :: ActivationFunction = Neurons.Identity(),
+    neuron :: ActivationFunction = Mocha.Neurons.Identity(),
 )
 
-Mocha.@characterize_layer(ScatteringLayer,
+Mocha.@characterize_layer(WaveletLayer,
     has_param => false,
     has_neuron => true,
     can_do_bp => true
