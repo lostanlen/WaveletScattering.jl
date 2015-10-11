cb2ca79795a1891aa05724e1fb813bb23b32292f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-11 23:07:19 +0200

    whitespace

--- src/blob.jl
+++ src/blob.jl
@@ -1,9 +1,13 @@
 # ScatteredBlob
-immutable ScatteredBlob{T<:Number, N} <: Mocha.Blob{T, N}
-    nodes::Dict{Path, AbstractNode{T, N}}
+immutable ScatteredBlob{T<:Number,N} <: Mocha.Blob{T, N}
+    nodes::Dict{Path, AbstractNode{T,N}}
     subscripts::NTuple{N, PathKey}
 end
 
 function fft!(blob::ScatteredBlob, dims)
     pmap(pair -> (pair.first, fft!(pair.second, dims)), blob)
+end
+
+function ScatteredBlob{T<:Number,N} <: Mocha.Blob{T, N}
+    
 end
