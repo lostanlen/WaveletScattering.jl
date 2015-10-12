371c419cb35bee9dda45090bde51b3b8d8372ba0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-12 11:57:34 +0200

    write scatteredblob outer constructor

--- src/blob.jl
+++ src/blob.jl
@@ -1,13 +1,16 @@
 # ScatteredBlob
 immutable ScatteredBlob{T<:Number,N} <: Mocha.Blob{T, N}
     nodes::Dict{Path, AbstractNode{T,N}}
-    subscripts::NTuple{N, PathKey}
+    subscripts::NTuple{N,PathKey}
 end
 
 function fft!(blob::ScatteredBlob, dims)
     pmap(pair -> (pair.first, fft!(pair.second, dims)), blob)
 end
 
-function ScatteredBlob{T<:Number,N} <: Mocha.Blob{T, N}
-    
+function ScatteredBlob{T<:Number,N}(x::AbstractArray{T,N},
+                                    subscripts::NTuple{N,PathKey})
+    emptypath = Dict{PathKey,Int}()
+    nodes = Dict{Path, AbstractNode{T,N}}(emptypath => x)
+    ScatteredBlob{T,N}(nodes, subcsripts)
 end
