fb6d0a492ce86aa8869b71adc5fb81d7afe98528
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-15 10:02:52 +0200

    define InplaceFourierNode and constructor

--- src/node.jl
+++ src/node.jl
@@ -1,12 +1,17 @@
 # Node
 abstract AbstractNode{T, N}
 
-immutable FourierNode{T<:Number,N} <: AbstractNode
-    data::AbstractArray{T,N}
-    data_ft::AbstractArray
+immutable InplaceFourierNode{T<:Number,N} <: AbstractNode
+    data::Array{T,N}
+    fourierdims::Tuple{Vararg{Int}}
     ranges::NTuple{N, PathRange}
 end
 
-function fft!(node::FourierNode, dims)
-    node.data_ft[:] = fft(node.data, dims)
+function InplaceFourierNode{T<:Complex}(data::Array{T},
+                                         fourierdims::Vararg{Tuple{Int}},
+                                         subscripts::NTuple{N, PathKey})
+    ranges = ntuple(k -> PathRange(subscripts(k),1:size(data,k)), ndims(data))
+    FourierNode(data, fourierdims, ranges)
 end
+InplaceFourierNode{T<:Number}(data::Array{T}, fourierdims, subscripts) =
+    InplaceFourierNode(complex(data), fourierdims, subscripts)
