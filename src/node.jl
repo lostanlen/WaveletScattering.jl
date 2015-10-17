a24aa7b6913edcf2eeea25c7c4de8ed5ebed4ce4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-17 11:26:02 +0200

    update src/node.jl

--- src/node.jl
+++ src/node.jl
@@ -1,17 +1,17 @@
 # Node
 abstract AbstractNode{T, N}
 
-immutable InplaceFourierNode{T<:Number,N} <: AbstractNode
+immutable InplaceFourierNode{T<:Complex,N} <: AbstractNode
     data::Array{T,N}
     fourierdims
     ranges::NTuple{N, PathRange}
 end
 
-function InplaceFourierNode{T<:Complex,N}(data::Array{T,N},
+function InplaceFourierNode{T<:Number,N}(data::Array{T,N},
                                          fourierdims,
                                          subscripts::NTuple{N, PathKey})
     ranges = ntuple(k -> PathRange(subscripts(k),1:size(data,k)), ndims(data))
-    FourierNode(data, fourierdims, ranges)
+    InplaceFourierNode(complex(data), fourierdims, ranges)
 end
 
 fft!(node::InplaceFourierNode) = fft(node.data, node.fourierdims)
--- test/blob.jl
+++ test/blob.jl
@@ -1,3 +1,6 @@
+# layer.jl
+import WaveletScattering: InplaceFourierNode, PathKey
+
 subscripts = (PathKey(:time), PathKey(:chunk))
 x = rand(Float32, 1024, 16)
-node = FourierNode(rand(Float32, 1024, 10), 1, subscripts)
+node = InplaceFourierNode(x, 1, subscripts)
