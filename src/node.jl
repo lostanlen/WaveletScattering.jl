cc4e66194c400278a4fa89325f99fdafd88e3ae2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-19 11:06:29 +0200

    rename InplaceFourierNode to FourierNode

--- src/node.jl
+++ src/node.jl
@@ -1,18 +1,17 @@
 # Node
 abstract AbstractNode{T, N}
 
-immutable InplaceFourierNode{T<:Complex,N} <: AbstractNode
+immutable FourierNode{T<:Complex,N} <: AbstractNode
     data::Array{T,N}
     fourierdims
     ranges::NTuple{N, PathRange}
 end
 
-function InplaceFourierNode{T<:Number,N}(data::Array{T,N},
-                                         fourierdims,
-                                         subscripts::NTuple{N, PathKey})
+function FourierNode{T<:Number,N}(data::Array{T,N}, fourierdims,
+                                  subscripts::NTuple{N, PathKey})
     ranges =
         ntuple(k -> PathRange(subscripts[k] => (1:size(data,k))), ndims(data))
-    InplaceFourierNode(complex(data), fourierdims, ranges)
+    FourierNode(complex(data), fourierdims, ranges)
 end
 
-Base.fft!(node::InplaceFourierNode) = fft!(node.data, node.fourierdims)
+Base.fft!(node::FourierNode) = fft!(node.data, node.fourierdims)
