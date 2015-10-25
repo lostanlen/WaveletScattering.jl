2093870b82924836eee99e548dc44633d6f8b513
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-25 18:31:21 +0100

    update test/blob.jl

--- test/blob.jl
+++ test/blob.jl
@@ -1,6 +1,9 @@
 # layer.jl
-import WaveletScattering: InplaceFourierNode, PathKey
+import WaveletScattering: FourierNode, PathKey
 
+# node
 subscripts = (PathKey(:time), PathKey(:chunk))
 x = rand(Float32, 1024, 16)
 node = InplaceFourierNode(x, 1, subscripts)
+allocated = @allocated fft!(node)
+@test allocated < 4000
