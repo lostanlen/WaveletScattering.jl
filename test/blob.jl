0fc42c226b023fa937bfc17263b87a9d164363fb
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-27 19:04:30 +0100

    more tests in test/blob.jl

--- test/blob.jl
+++ test/blob.jl
@@ -1,7 +1,11 @@
+# bank.jl
+import WaveletScattering: FourierNonOriented1DBank
+# morlet1d.jl
+import WaveletScattering: Morlet1DSpec
 # layer.jl
 import WaveletScattering: PathKey
 # node.jl
-import WaveletScattering.AbstractFourierNode
+import WaveletScattering: AbstractFourierNode, InverseFourierNode
 
 # node
 subscripts = (PathKey(:time), PathKey(:chunk))
@@ -10,3 +14,12 @@ node = AbstractFourierNode(data, 1, subscripts)
 fft!(node) # warm-up
 allocated = @allocated fft!(node)
 @test allocated == 0
+
+spec = Morlet1DSpec()
+bank = FourierNonOriented1DBank(spec)
+
+data_out = zeros(Complex{Float32}, 32768, 256)
+inverse_plan = plan_ifft!(data_out, 1)
+node_out = InverseFourierNode(data_out, inverse_plan, node.ranges)
+γ = 0
+ψ = bank.ψs[1 + γ]
