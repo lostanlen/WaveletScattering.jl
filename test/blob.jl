94d78ecb910621c60545cb7c5ac3582d3dea9cfe
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-27 16:33:30 +0100

    update test/blob.jl

--- test/blob.jl
+++ test/blob.jl
@@ -1,9 +1,12 @@
 # layer.jl
 import WaveletScattering: PathKey
+# node.jl
+import WaveletScattering.AbstractFourierNode
 
 # node
 subscripts = (PathKey(:time), PathKey(:chunk))
-data = rand(Float32, 32768, 128)
+data = rand(Float32, 32768, 256)
 node = AbstractFourierNode(data, 1, subscripts)
+fft!(node) # warm-up
 allocated = @allocated fft!(node)
-@test allocated < 4000
+@test allocated == 0
