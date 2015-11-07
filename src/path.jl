bef69d2e5c74226a75022ceb3e4c1230737e5c1b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-07 19:17:16 +0100

    PathKey's are now double-ended queues of Literal's

--- src/path.jl
+++ src/path.jl
@@ -7,14 +7,14 @@ typealias SymbolInt Tuple{Symbol,Int}
 Literal(tup::SymbolInt) = Literal(tup[1], tup[2])
 Literal(sym::Symbol) = Literal(sym, 1)
 
-"A `PathKey` is a linked list of `Literal`s"
+"A `PathKey` is a double-ended queue (Deque) of `Literal`s"
 type PathKey
-    list::LinkedList{Literal}
-    PathKey() = new(nil(Literal))
-    PathKey(args...) = new(map(Literal, list(args...)))
+    list::DataStructures.Deque{Literal}
+    PathKey() = new(DataStructures.Deque{Literal}())
+    PathKey(x, args...) = DataStructures.unshift!(PathKey(args...), Literal(x))
 end
 
-"""A `Path` is a dictionary whose keys `PathKeys` and whose values are
+"""A `Path` is a dictionary whose keys are `PathKey`s and whose values are
 integer indices"""
 typealias Path Dict{PathKey, Int}
 
