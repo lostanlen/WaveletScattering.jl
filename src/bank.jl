0795d3d5ef7be02626acc1e217c97e47bac2cb8f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-07 15:41:21 +0100

    call new Behavior outer constructor

--- src/bank.jl
+++ src/bank.jl
@@ -81,9 +81,9 @@ immutable FourierNonOriented1DBank{T<:FFTW.fftwNumber} <:
         ψs = convert(Array{AbstractFourier1DFilter{T}}, ψs)
         ϕ = scalingfunction(spec)
         renormalize!(ψs, ϕ, metas, spec)
-        behavior =
-            Behavior(γ_range, is_ϕ_applied, log2_oversampling, max_log2_stride)
         γ_range = max(γ_range.start, 0):min(γ_range.stop, length(γs) - 1)
+        behavior = Behavior(ϕ, ψs,
+            γ_range, is_ϕ_applied, log2_oversampling, max_log2_stride)
         new{T}(ϕ, ψs, behavior, metas, spec)
     end
 end
@@ -121,6 +121,8 @@ immutable FourierOriented1DBank{T<:FFTW.fftwNumber} <: AbstractOrientedBank{T}
         ϕ = scalingfunction(spec)
         renormalize!(ϕ, ψs, metas, spec)
         γ_range = max(γ_range.start, 0):min(γ_range.stop, length(γs) - 1)
+        behavior = Behavior(ϕ, ψs,
+            γ_range, is_ϕ_applied, log2_oversampling, max_log2_stride)
         new{T}(ϕ, ψs, behavior, metas, spec)
     end
 end
