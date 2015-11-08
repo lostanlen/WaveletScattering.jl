ae66671b1523b8ffd12a90baebda3edd131daa69
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-08 22:03:37 +0100

    lighten up src/bank.jl

--- src/bank.jl
+++ src/bank.jl
@@ -1,47 +1,3 @@
-"""A `Behavior` object contains the mutable information in a filter bank. The
-values of these fields may be changed *after* the construction of the filter
-bank, without having to recompute the underlying architecture. Fields:
-
-* `is_ϕ_applied`: true if and only if the lowpass filter `ϕ` (also known as
-scaling function) in addition to the bandpass filters `ψ`'s.
-
-* `j_range`: range of wavelet octaves that are actually used in the
-convolutions. Default is all of them.
-
-* `log2_oversampling`: base-2 logarithm of the oversampling factor with respect
-to the critical sampling rate. Must be positive. Default is 0, i.e. no
-oversampling.
-
-* `max_log2_stride`: base-2 logarithm of the maximum distance between
-neighboring coefficients after subsampling (also known as *hop size* or
-*stride*). Must be positive. Default is the number of octaves minus one, which
-imposes no oversampling per se."""
-type Behavior
-    ϕ_log2_sampling::Int
-    ψ_log2_samplings::Vector{Int}
-    is_ϕ_applied::Bool
-    j_range::UnitRange
-    log2_oversampling::Int
-    max_log2_stride::Int
-end
-
-function Behavior{T}(ϕ::Symmetric1DFilter{T},
-        ψs::AbstractArray{AbstractFourierFilter{T}},
-        spec::AbstractSpec, is_ϕ_applied::Bool, j_range::UnitRange{Int},
-        log2_oversampling::Int, max_log2_stride::Int)
-    ϕ_critical_log2_sampling = critical_log2_sampling(ϕ, spec)
-    ϕ_log2_sampling =
-        clamp(ϕ_critical_log2_sampling + log2_oversampling, -max_log2_stride, 0)
-    edge_ids = (j_range+1) * spec.nFilters_per_octave - 1
-    ψ_critical_log2_samplings =
-        Int[ critical_log2_sampling(ψ, spec) for ψ in ψs[edge_ids, 1] ]
-    ψ_log2_samplings = clamp(ψ_critical_log2_samplings + log2_oversampling,
-        -max_log2_stride, 0)
-    max_log2_stride = - min(ϕ_log2_sampling, minimum(ψ_log2_samplings))
-    Behavior(ϕ_log2_sampling, ψ_log2_samplings, is_ϕ_applied, j_range,
-        log2_oversampling, max_log2_stride)
-end
-
 """An `AbstractBank` is a wavelet filter bank. Filter banks are of two kinds:
 * `AbstractNonOrientedBank`: no orientation variable `θ`, only a scale
 variable `γ`. One-dimensional filter banks with real inputs are non-oriented.
@@ -50,9 +6,6 @@ variable `γ`. Two-dimensional filter banks are oriented. One-dimensional filter
 banks with complex outputs are also oriented, in the sense that the sign
 of the center frequency is regarded as an orientation variable with two
 possible values."""
-abstract AbstractBank{T<:Number}
-abstract AbstractNonOrientedBank{T<:Number} <: AbstractBank{T}
-abstract AbstractOrientedBank{T<:Number} <: AbstractBank{T}
 
 """A `FourierNonOriented1DBank` is a one-dimensional, non-oriented filter bank
 defined in the Fourier domain. It is ""non-oriented"" in the sense that only the
@@ -61,18 +14,14 @@ input signal will be real — i.e. its Fourier will be symmetric — it can be
 recovered from the ""positive half"" of its Fourier spectrum. It is
 advisable to use this type of filter bank when handling real 1d data of
 moderate to large length."""
-immutable FourierNonOriented1DBank{T<:FFTW.fftwNumber} <:
-        AbstractNonOrientedBank{T}
-    ϕ::Symmetric1DFilter{T}
-    ψs::Vector{AbstractFourierFilter{T,1}}
-    behavior::Behavior
-    metas::Vector{NonOrientedMeta}
-    spec::Abstract1DSpec{T}
-    function call{T<:FFTW.fftwNumber}(
-            ::Type{FourierNonOriented1DBank{T}}, spec::Abstract1DSpec ;
-            is_ϕ_applied::Bool = false,
-            j_range::UnitRange{Int} = 0:(spec.nOctaves-1),
-            log2_oversampling::Int = 0, max_log2_stride::Int = spec.nOctaves-1)
+immutable Bank{T<:FFTW.fftwNumber,D<:AbstractDomain,G<:AbstractPointGroup}
+    ϕ::AbstractFilter{T,D,G}
+    ψs::Array{AbstractFilter{T,D,G},3}
+    behavior::Behavior{D}
+    metas::Vector{Meta{G}}
+    spec::AbstractSpec{T,G}
+    function call{T<:FFTW.fftwNumber}(::Type{FourierNonOriented1DBank{T}},
+        spec::AbstractSpec{T,D}, behavior::Behavior{D,G})
         T == spec.signaltype || error("""Type parameter of
         FourierNonOriented1DBankmust must be equal to spec.signaltype""")
         γs, χs, js = gammas(spec), chromas(spec), octaves(spec)
