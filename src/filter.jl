f6bab72ccbbb7e5368192e68bf5735e24b57ac27
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-08 22:04:10 +0100

    update src/filter.jl

--- src/filter.jl
+++ src/filter.jl
@@ -1,6 +1,18 @@
-abstract AbstractFilter{T<:Number,K}
+abstract AbstractFilter{T<:Number,D<:AbstractDomain}
 
-immutable Symmetric1DFilter{T<:Number} <: AbstractFilter{T,1}
+immutable Symmetric1DFilter{T<:Number} <: AbstractFilter{T,SpatialDomain{1}}
     leg::Vector{T}
     zero::T
 end
+
+immutable Asymmetric1DFilter{T<:Number} <: AbstractFilter{T,SpatialDomain{1}}
+    neg::Vector{T}
+    pos::Vector{T}
+    zero::T
+end
+
+# product is commutative
+Base.(:*)(b::Number, ψ::AbstractFilter) = ψ * b
+
+# element-wise multiplication operator ".*" with scalar falls back to "*"
+Base.(:.*)(ψ::AbstractFilter, b::Number) = ψ * b
