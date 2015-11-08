3d7b01c8a589b73c77b90ea45ca3875fd2421be5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-08 22:04:20 +0100

    rename src/group.jl to src/group.jl

--- src/group.jl
+++ src/group.jl
@@ -2,3 +2,5 @@ abstract AbstractPointGroup
 immutable TrivialGroup <: AbstractPointGroup end
 immutable ReflectionGroup <: AbstractPointGroup end
 immutable RotationGroup <: AbstractPointGroup end
+
+typealias LineGroups Union{TrivialGroup,ReflectionGroup}
