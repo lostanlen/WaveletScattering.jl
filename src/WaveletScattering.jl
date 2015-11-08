c8e62b8878f85bb7653fccc90843652bcb2c5202
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-08 22:05:06 +0100

    include new files in WaveletScattering.jl main file

--- src/WaveletScattering.jl
+++ src/WaveletScattering.jl
@@ -6,10 +6,12 @@ using JuMP
 using MathProgBase
 using Mocha
 
+include("domain.jl")
+include("group.jl")
 include("spec.jl")
 include("meta.jl")
 include("filter.jl")
-include("fourierfilter.jl")
+include("fourier1dfilter.jl")
 include("bank.jl")
 include("morlet1d.jl")
 include("path.jl")
