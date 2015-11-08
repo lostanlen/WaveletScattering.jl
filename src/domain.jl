119860c2ab0256600fea19347a7af27c7968425d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-08 22:03:55 +0100

    define typealiases LineDomains and PlaneDomains

--- src/domain.jl
+++ src/domain.jl
@@ -8,3 +8,6 @@ immutable SpatialDomain{N} <: AbstractDomain
     dim::Val{N}
 end
 SpatialDomain(dim::Int) = SpatialDomain{dim}(Val{dim}())
+
+typealias LineDomains Union{FourierDomain{1},SpatialDomain{1}}
+typealias PlaneDomains Union{FourierDomain{2},SpatialDomain{2}}
