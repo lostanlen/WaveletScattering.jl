32471219c5ade624802776e14cf70ec5ae63ba62
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-08 22:05:29 +0100

    define pathdepth method

--- src/blob.jl
+++ src/blob.jl
@@ -18,7 +18,7 @@ Base.fft!(blob::AbstractFourierBlob) = pmap(fft!, values(blob.nodes))
 Base.ifft!(blob::AbstractFourierBlob) = pmap(ifft!, values(blob.nodes))
 
 pathdepth(path::Path, refkey::PathKey) =
-    mapreduce(key -> pathdepth(path, refkey), max, 1, keys(path))
+    mapreduce(path -> pathdepth(path, refkey), max, 1, keys(path))
 
 function pathdepth(key::PathKey, refkey::PathKey)
     while ~isempty(key) && ~isempty(refkey) && (back(key) == back(refkey))
@@ -31,15 +31,29 @@ function pathdepth(key::PathKey, refkey::PathKey)
     end
     return 1
 end
+
+function pathdepth(blob::AbstractFourierBlob, refkey::PathKey)
+    anypath = keys(blob.nodes)[1]
+    pathdepth_dictlevel = pathdepth(anypath)
+    pathdepth_tensorlevel = pathdepth(blob.nodes[anypath])
+    return max(pathdepth_dictlevel, pathdepth_tensorlevel)
+end
+
 function forward!(backend::Mocha.CPUBackend, blob_out::AbstractScatteredBlob,
                   bank::AbstractNonOrientedBank, blob_in::AbstractScatteredBlob)
+    pathdepth(blob_in, bank.behavior.pathkey)
+
+    map(node -> pathdepth(bank.behavior.pathkey, keys(blob_in.nodes))
     γkey = cons(Literal(:γ, 1), bank.behavior.pathkey)
-    for γ in 0:(nGammas-1)
-        ψ = bank.ψs[1 + γ]
-        for (path_in, node_in) in input.nodes
-            path_out = copy(path_in)
-            path_out[γkey] = γ
-            transform!(blob[path_out], blob_in[path_in], ψ)
+    for j in bank.behavior.j_range
+        for χ in 0:(bank.spec.nFilters_per_octave-1)
+            γ = j * nFilters_per_octave + χ
+            ψ = bank.ψs[1 + γ]
+            for (path_in, node_in) in input.nodes
+                path_out = copy(path_in)
+                path_out[γkey] = γ
+                transform!(blob[path_out], blob_in[path_in], ψ)
+            end
         end
     end
 end
