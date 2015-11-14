7bd8122d4a05fa80277ef5cf7e22ee682fe5d47c
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-14 11:57:12 +0100

    refactor AbstractScatteredBlob type

--- src/blob.jl
+++ src/blob.jl
@@ -1,6 +1,5 @@
 # ScatteredBlob
-abstract AbstractScatteredBlob{T,N} <: Mocha.Blob{T,N}
-abstract AbstractFourierBlob{T<:FFTW.fftwNumber,N} <: AbstractScatteredBlob{T,N}
+abstract AbstractScatteredBlob{T,D<:AbstractDomain,N} <: Mocha.Blob{T,N}
 
 immutable RealFourierBlob{T<:FFTW.fftwReal,N} <: AbstractFourierBlob{T,N}
     nodes::Dict{Path,RealFourierNode{T,N}}
