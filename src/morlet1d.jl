f37ce6ea9c69d7d4687b313f8afe9470d0c56492
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-17 10:12:11 +0100

    generalize Morlet1DSpec to Spec1D

--- src/morlet1d.jl
+++ src/morlet1d.jl
@@ -1,17 +1,18 @@
-immutable Morlet1DSpec{T<:FFTW.fftwReal,D<:LineDomains,G<:LineGroups} <:
-        AbstractSpec{T,D,G}
+immutable Spec1D{T<:FFTW.fftwReal,D<:LineDomains,
+        G<:LineGroups,W<:RedundantWaveletClass} <: AbstractSpec{T,D,G}
     ɛ::Float64
-    domain::D
+    class::Type{W}
+    domain::Type{D}
     log2_size::Tuple{Int}
     max_qualityfactor::Float64
     max_scale::Float64
     motherfrequency::Float64
     nFilters_per_octave::Int
     nOctaves::Int
-    pointgroup::G
+    pointgroup::Type{G}
     signaltype::Type{T}
     function call{T<:FFTW.fftwReal,D<:LineDomains,G<:LineGroups}(
-            ::Type{Morlet1DSpec}, ::Type{T} = Float32,
+            ::Type{Spec1D}, ::Type{W} = Morlet, ::Type{T} = Float32,
             ::Type{D} = FourierDomain{1}, ::Type{G} = TrivialGroup;
             ɛ = default_ɛ(T), log2_size = 15, max_qualityfactor = nothing,
             max_scale = Inf, nFilters_per_octave = nothing, nOctaves = nothing,
@@ -21,13 +22,12 @@ immutable Morlet1DSpec{T<:FFTW.fftwReal,D<:LineDomains,G<:LineGroups} <:
         max_qualityfactor, nFilters_per_octave =
              default_max_qualityfactor(max_qualityfactor, nFilters_per_octave),
              default_nFilters_per_octave(nFilters_per_octave, max_qualityfactor)
-        motherfrequency = tune_motherfrequency(tuningfrequency, Morlet1DSpec,
-                                               nFilters_per_octave)
-        nOctaves = default_nOctaves(nOctaves, Morlet1DSpec, log2_size,
-                                    max_qualityfactor, max_scale,
-                                    motherfrequency, nFilters_per_octave)
-        spec = new{T}(ɛ, log2_size, max_qualityfactor, max_scale,
-                      motherfrequency, nFilters_per_octave, nOctaves, T)
+        motherfrequency =
+            tune_motherfrequency(tuningfrequency, W, nFilters_per_octave)
+        nOctaves = default_nOctaves(nOctaves, W, log2_size,
+            max_qualityfactor, max_scale, motherfrequency, nFilters_per_octave)
+        spec = new{T,D,G,W}(ɛ, W, D, log2_size, max_qualityfactor, max_scale,
+            motherfrequency, nFilters_per_octave, nOctaves, G, T)
         checkspec(spec) && return spec
     end
 end
