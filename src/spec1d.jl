2de4b4afe22e57c0e041e2e3719e9dd3853405c2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-17 14:36:23 +0100

    update spec1d.jl

--- src/spec1d.jl
+++ src/spec1d.jl
@@ -1,19 +1,21 @@
 immutable Spec1D{T<:FFTW.fftwReal,D<:LineDomains,
         G<:LineGroups,W<:RedundantWaveletClass} <: AbstractSpec{T,D,G}
     ɛ::Float64
-    class::Type{W}
-    domain::Type{D}
+    class::W
+    domain::D
     log2_size::Tuple{Int}
     max_qualityfactor::Float64
     max_scale::Float64
     motherfrequency::Float64
     nFilters_per_octave::Int
     nOctaves::Int
-    pointgroup::Type{G}
+    pointgroup::G
     signaltype::Type{T}
-    function call{T<:FFTW.fftwReal,D<:LineDomains,G<:LineGroups}(
-            ::Type{Spec1D}, ::Type{W} = Morlet, ::Type{T} = Float32,
-            ::Type{D} = FourierDomain{1}, ::Type{G} = TrivialGroup;
+    function call(::Type{Spec1D},
+            class::RedundantWaveletClass = Morlet,
+            pointgroup::LineGroups = TrivialGroup,
+            signaltype = Float32,
+            domain::LineDomains = FourierDomain{1} ;
             ɛ = default_ɛ(T), log2_size = 15, max_qualityfactor = nothing,
             max_scale = Inf, nFilters_per_octave = nothing, nOctaves = nothing,
             tuningfrequency = nothing)
@@ -23,11 +25,12 @@ immutable Spec1D{T<:FFTW.fftwReal,D<:LineDomains,
              default_max_qualityfactor(max_qualityfactor, nFilters_per_octave),
              default_nFilters_per_octave(nFilters_per_octave, max_qualityfactor)
         motherfrequency =
-            tune_motherfrequency(tuningfrequency, W, nFilters_per_octave)
-        nOctaves = default_nOctaves(nOctaves, W, log2_size,
+            tune_motherfrequency(tuningfrequency, class, nFilters_per_octave)
+        nOctaves = default_nOctaves(nOctaves, class, log2_size,
             max_qualityfactor, max_scale, motherfrequency, nFilters_per_octave)
-        spec = new{T,D,G,W}(ɛ, W, D, log2_size, max_qualityfactor, max_scale,
-            motherfrequency, nFilters_per_octave, nOctaves, G, T)
+        spec = new(ɛ, class, domain, log2_size, max_qualityfactor, max_scale,
+            motherfrequency, nFilters_per_octave, nOctaves, pointgroup,
+            signaltype)
         checkspec(spec) && return spec
     end
 end
