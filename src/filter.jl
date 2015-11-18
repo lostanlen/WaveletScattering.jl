a98614b20846b2b5aaef66b6ee2e2b368a8c67a4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-18 16:49:53 +0100

    write spin!(ψs::Array{AbstractFilter,3})

--- src/filter.jl
+++ src/filter.jl
@@ -16,3 +16,10 @@ Base.(:*)(b::Number, ψ::AbstractFilter) = ψ * b
 
 # element-wise multiplication operator ".*" with scalar falls back to "*"
 Base.(:.*)(ψ::AbstractFilter, b::Number) = ψ * b
+
+function spin!(ψs::Array{AbstractFilter,3})
+    (nΘs, nΧs, nJs) = size(ψs)
+    idγs = range(1, nΘs, nΘ * nΧs * nJs)
+    # Caution: partial linear indexing may be deprecated in the future
+    pmap(idγ -> spin!(ψs[2:end, idγ], ψs[1, idγ]), idγs)
+end
