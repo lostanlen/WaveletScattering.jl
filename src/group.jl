ffd9fc1bd1da5f0c0b043fd99bffe3c350675d53
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-18 09:04:50 +0100

    write get_nOrientations

--- src/group.jl
+++ src/group.jl
@@ -1,6 +1,12 @@
 abstract AbstractPointGroup
 immutable TrivialGroup <: AbstractPointGroup end
 immutable ReflectionGroup <: AbstractPointGroup end
-immutable RotationGroup <: AbstractPointGroup end
+immutable RotationGroup <: AbstractPointGroup
+    nOrientations::Int
+end
 
 typealias LineGroups Union{TrivialGroup,ReflectionGroup}
+
+get_nOrientations(group::TrivialGroup) = 1
+get_nOrientations(group::ReflectionGroup) = 2
+get_nOrientations(group::RotationGroup) = group.nOrientations
