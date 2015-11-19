5b9060a79a13347100af31f3564400bee9e2e435
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-19 12:25:04 +0100

    replace pmap by a double loop in spin!

--- src/filter.jl
+++ src/filter.jl
@@ -19,7 +19,9 @@ Base.(:.*)(ψ::AbstractFilter, b::Number) = ψ * b
 
 function spin!{T,D}(ψs::Array{AbstractFilter{T,D},3})
     (nΘs, nΧs, nJs) = size(ψs)
-    idγs = range(1, nΘs, nΘ * nΧs * nJs)
-    # Caution: partial linear indexing may be deprecated in the future
-    pmap(idγ -> spin!(ψs[2:end, idγ], ψs[1, idγ]), idγs)
+    for χ in 0:(nΧs-1)
+        for j in 0:(nJs-1)
+            ψs[2:end, 1+χ, 1+j] = spin(ψs[1, 1+χ, 1+j])
+        end
+    end
 end
