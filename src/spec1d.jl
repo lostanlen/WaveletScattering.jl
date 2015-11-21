893e72d7a2a3c3767d5e76356d16735da2975aec
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-21 16:01:14 +0100

    document Spec1D constructor

--- src/spec1d.jl
+++ src/spec1d.jl
@@ -26,17 +26,34 @@ immutable Spec1D{T<:Real,D<:LineDomains,
             nFilters_per_octave = nothing,
             nOctaves = nothing,
             tuningfrequency = nothing)
-        "Integer log2_size is automatically converted to one-element tuple"
+        "Integer `log2_size` is automatically converted to one-element tuple"
         isa(log2_size, Int) && (log2_size = tuple(log2_size))
+        """Maximum quality factor and number of filters per octave are
+        set simultaneously, so that they default to each other if either
+        is present."""
         max_qualityfactor, nFilters_per_octave =
              default_max_qualityfactor(max_qualityfactor, nFilters_per_octave),
              default_nFilters_per_octave(nFilters_per_octave, max_qualityfactor)
+        """The center frequency of the mother wavelet (`motherfrequency`)
+        may be tuned to user-defined frequency (e.g. 440 Hz in music) or left
+        as a default."""
         motherfrequency =
             tune_motherfrequency(tuningfrequency, class, nFilters_per_octave)
+        """By default, the number of octaves in the filter bank is such that
+        all wavelet scales do not exceed the signal length, nor the
+        user-defined maximum scale `max_scale` if present."""
         nOctaves = default_nOctaves(nOctaves, class, log2_size,
             max_qualityfactor, max_scale, motherfrequency, nFilters_per_octave)
+        """The number of orientations of a one-dimensional filter bank is
+        equal to 1 for real input (`poingroup::TrivialGroup`), equal to 2 for
+        complex input (`pointgroup::ReflectionGroup`)."""
         nΘs = get_nOrientations(pointgroup)
         ħ = uncertainty(class)
+        """The meta-information of the wavelets is computed as a 3-d array,
+        whose dimensions respectively correspond to
+        * the orientation variable `θ`,
+        * the chroma variable `χ`, and
+        * the octave variable `j`."""
         ψmetas = Array{ΨMeta}(nΘs, nFilters_per_octave, nOctaves)
         for j in 0:(nOctaves-1), χ in 0:(nFilters_per_octave-1)
             γ = j * nFilters_per_octave + χ
@@ -52,12 +69,18 @@ immutable Spec1D{T<:Real,D<:LineDomains,
                     centerfrequency, j, qualityfactor, scale)
             end
         end
+        """The bandwidth of the lowpass filter `ϕ` is such that the Fourier
+        transform of `ϕ` will cover the low-frequency part of the spectrum
+        that is not covered by the wavelets `ψs`."""
         ϕbandwidth = motherfrequency * exp2(-nOctaves)
         ϕscale = ħ / ϕbandwidth
         ϕmeta = ΦMeta(ϕbandwidth, ϕscale)
         spec = new{T,D,G,W}(ɛ, ϕmeta, ψmetas, class, domain, log2_size,
             max_qualityfactor, max_scale, motherfrequency, nFilters_per_octave,
             nOctaves, pointgroup, signaltype)
+        """Before returning the `spec`, we call the function `checkspec`
+        which enforces properties of the wavelet filter bank to satisfy null
+        mean, limited spatial support, and Littlewood-Paley inequality."""
         checkspec(spec) && return spec
     end
 end
