8e0820b3e56fa16794ef2cd6a6134fe9d35ddd73
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-22 20:41:49 +0100

    write pointwise modulus methods and types

--- src/pointwise.jl
+++ src/pointwise.jl
@@ -1,5 +1,28 @@
 abstract AbstractPointwise
 immutable Modulus <: AbstractPointwise end
 
-map!(ρ::Modulus, blob_in::AbstractNode, blob_out::AbstractNode) =
-    map!(abs, blob_in.data, blob_out.data)
+Mocha.@defstruct PointwiseLayer Mocha.Layer (
+    name :: AbstractString = "pointwise",
+    (bottoms :: Vector{Symbol} = Symbol[], length(bottoms) > 0),
+    (tops :: Vector{Symbol} = Symbol[], length(tops) == length(bottoms)),
+)
+
+immutable PointwiseLayerState{BLOB<:ScatteredBlob,P<:AbstractPointwise}
+    layer::PointwiseLayer
+    blobs::Vector{BLOB}
+    blobs_diff::Any
+
+    ρ::P
+end
+
+function forward{B<:ScatteredBlob}(
+        backend::Mocha.CPUBackend,
+        layerstate::PointwiseLayerState,
+        inputs::Vector{B})
+    for id in eachindex(inputs)
+        map!(layerstate.ρ, layerstate.blobs[id], inputs[id])
+    end
+end
+
+map!(ρ::Modulus, blob_out::AbstractNode, blob_in::AbstractNode) =
+    map!(abs, blob_out.data, blob_in.data)
