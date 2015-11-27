43d41d53e53520af5e1dde0a2a1d3615e55d21e1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-27 11:49:39 +0100

    document ΦMeta

--- src/meta.jl
+++ src/meta.jl
@@ -5,7 +5,7 @@ an oriented wavelet within a filter bank. Fields:
 * `χ` chroma. `χ` is `mod(γ, nFilters_per_octave)`
 * `j` octave. `j` is `div(γ, nFilters_per_octave)`
 * `bandwidth` ∈]0,1] the width at -3dB, expressed in fraction of signal length
-* `centerfrequency` ∈]0,1] expressed in fraction of signal length
+* `centerfrequency` ∈]0,1] is expressed in fraction of signal length
 * `qualityfactor` ∈[1,max_qualityfactor] is equal to `centerfrequency/bandwidth`
 * `scale` is the FWTM (full width at tenth maximum) in spatial domain"""
 immutable ΨMeta
@@ -19,6 +19,10 @@ immutable ΨMeta
     scale::Float64
 end
 
+"""A `ΦMeta` object contains all the meta-information to identify a low-pass
+filter within a wavelet filter bank. Fields:
+* `bandwidth` ∈]0,1] the width at -3dB, expressed in fraction of signal length
+* `scale` ∈]0,1] is the FWTM (full width at tenth maximum) in spatial domain."""
 immutable ΦMeta
     bandwidth::Float64
     scale::Float64
