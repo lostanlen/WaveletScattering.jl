b8449fb3b0db985e6a71dbdecc146ef2ef5776b5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-27 11:40:10 +0100

    document AbstractPointGroup

--- src/group.jl
+++ src/group.jl
@@ -1,3 +1,13 @@
+"""Three variables come into play in a wavelet filterbank :
+* the spatial variable V, which may be multi-dimensional ;
+* the scale variable γ::V, which is one-dimensional and non-negative ;
+* the isometric variable θ::V, which belongs to a ""point group"".
+A point group is an algebraic group of transformations which have a single
+fixed point. Reflections of the real line and rotations of the plane are
+examples of such point groups. Conversely, non-trivial translations and
+scalings have zero fixed points, so the transformation groups they belong are
+not point groups.
+"""
 abstract AbstractPointGroup
 immutable TrivialGroup <: AbstractPointGroup end
 immutable ReflectionGroup <: AbstractPointGroup end
