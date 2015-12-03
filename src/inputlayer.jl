4953224afb04dfc153229e52850a2fea780ba749
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-03 23:23:34 +0100

    write src/inputlayer.jl

---
+++ src/inputlayer.jl
@@ -0,0 +1,16 @@
+# InputLayer
+# We adopt the same whitespace convention as in the Mocha code base
+Mocha.@defstruct InputLayer Mocha.Layer (
+    name :: AbstractString = "scattering-input",
+    top :: Symbol,
+    data :: AbstractArray,
+    (symbols :: Vector{Symbol} = defaultsymbols(data),
+        length(symbols) == ndims(data))
+)
+
+Mocha.@characterize_layer(InputLayer,
+    has_neuron => false,
+    has_param => false,
+    can_do_bp => true,
+    is_source => true,
+)
