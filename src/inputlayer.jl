8e88f6aade75c11e9b8d13a45fd3d2c730c03ad2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-04 08:42:28 +0100

    default values in inputlayer.jl

--- src/inputlayer.jl
+++ src/inputlayer.jl
@@ -2,9 +2,9 @@
 # We adopt the same whitespace convention as in the Mocha code base
 Mocha.@defstruct InputLayer Mocha.Layer (
     name :: AbstractString = "scattering-input",
-    top :: Symbol,
-    data :: AbstractArray,
-    (symbols :: Vector{Symbol}, length(symbols) == ndims(data))
+    top :: Symbol = [:data],
+    data :: AbstractArray = [],
+    (symbols :: Vector{Symbol} = Symbol[], length(symbols) == ndims(data))
 )
 
 Mocha.@characterize_layer(InputLayer,
