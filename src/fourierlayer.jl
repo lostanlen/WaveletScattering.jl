d3fd1a05c9c3b92d4ee7e591df8cda86d00006d0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-10 13:34:59 +0100

    add variables field to FourierLayer

--- src/fourierlayer.jl
+++ src/fourierlayer.jl
@@ -4,6 +4,7 @@ Mocha.@defstruct FourierLayer Mocha.Layer (
     name :: AbstractString = "fourier",
     (bottoms :: Vector{Symbol} = Symbol[], length(bottoms) > 0),
     (tops :: Vector{Symbol} = Symbol[], length(bottoms) == length(tops)),
+    (variables :: Vector{Vector{Symbol}} = Vector{Symbol}[]),
     flags :: UInt32 = FFTW.ESTIMATE,
     timelimit :: Float64 = Inf,
 )
