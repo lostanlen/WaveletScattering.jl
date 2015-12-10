8ec048b2508f61a8918988b75b593927eea64abf
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-10 13:03:19 +0100

    write kthrange

--- src/inputlayerstate.jl
+++ src/inputlayerstate.jl
@@ -2,3 +2,11 @@ immutable InputLayerState{B<:ScatteredBlob} <: AbstractScatteredLayerState
     layer::InputLayer
     blob::B
 end
+
+function InputLayerState(backend::Backend, layer::InputLayer)
+    ranges = ntuple(k -> kthrange(layer, k), ndims(layer.data))
+    node = Node(layer.data, ranges)
+end
+
+kthrange(layer::InputLayer, k::Int) =
+    (layer.symbols[k] => (1:1:size(layer.data, k)))s
