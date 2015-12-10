ed706c4c36d8cfe25388a8e4300c3c02d439cea3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-10 18:40:16 +0100

    rewrite Deque equality with iterators

--- src/deque.jl
+++ src/deque.jl
@@ -1,5 +1,10 @@
-function Base.isequal(x::Deque, y::Deque)
-    DataStructures.isempty(x) && DataStructures.isempty(y) && return true
-    isequal(DataStructures.pop!(x), DataStructures.pop!(y)) || return false
-    isequal(x,y)
+function Base.(:(==))(x::Deque, y::Deque ; front::Bool = true)
+    itx = DataStructures.start(x)
+    ity = DataStructures.start(y)
+    while ~DataStructures.done(x, itx) && ~DataStructures.done(y, ity)
+        (valx, itx) = DataStructures.next(x, itx)
+        (valy, ity) = DataStructures.next(y, ity)
+        (valx == valy) || return false
+    end
+    return DataStructures.done(x, itx) && DataStructures.done(y, ity)
 end
