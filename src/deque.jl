4bc09df60fddbecbded9e8dd4b6c556904462ea5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-10 16:27:14 +0100

    update isequal for Deque's

--- src/deque.jl
+++ src/deque.jl
@@ -1,1 +1,5 @@
-Base.isequal(x::Deque, y::Deque) = isequal(pop!(x), pop!(y)) && isequal(x,y)
+function Base.isequal(x::Deque, y::Deque)
+    DataStructures.isempty(x) && DataStructures.isempty(y) && return true
+    isequal(DataStructures.pop!(x), DataStructures.pop!(y)) || return false
+    isequal(x,y)
+end
