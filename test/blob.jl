ab4d92daae88f0dea4a26d68dfd90fd75507aa33
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-23 17:43:40 +0100

    update test/blob.jl

--- test/blob.jl
+++ test/blob.jl
@@ -19,23 +19,6 @@ signal_layer =
     InputLayer(name = "signal", data = data, symbols = [:time, :chunk])
 signal_state = InputLayerState(backend, signal_layer)
 
-layer = FourierLayer("fourier", [:data], [:fourier],
+fourier_layer = FourierLayer("fourier", [:data], [:fourier],
     [PathKey(:time)], FFTW.ESTIMATE, Inf)
-
-
-node = RealFourierNode(data, [1], pathkeys)
-@test_approx_eq maximum(abs(imag(node.data[1,:]))) 0.0
-blob = ScatteredBlob(Dict(Path() => node), subscripts)
-
-
-bank = Bank1D(Spec1D(), PathKey(:time))
-
-
-
-
-
-data_out = zeros(Complex{Float32}, 32768, 256)
-inverse_plan = plan_ifft!(data_out, 1)
-node_out = InverseFourierNode(data_out, inverse_plan, node.ranges)
-γ = 0
-ψ = bank.ψs[1 + γ]
+fourier_state = FourierLayerState(backend, signal_layer, [signal_state])
