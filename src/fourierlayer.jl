281a74c85fedd152aa7a8b9507cebb111bc443ca
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-23 17:22:10 +0100

    define FourierLayer outer constructor without defstruct

--- src/fourierlayer.jl
+++ src/fourierlayer.jl
@@ -1,13 +1,23 @@
-# FourierLayer
-# We adopt the same whitespace convention as in the Mocha code base
-Mocha.@defstruct FourierLayer Mocha.Layer (
-    name :: AbstractString = "fourier",
-    (bottoms :: Vector{Symbol} = Symbol[], length(bottoms) > 0),
-    (tops :: Vector{Symbol} = Symbol[], length(bottoms) == length(tops)),
-    (pathkeys :: Vector{PathKey} = PathKey[]),
-    flags :: UInt32 = FFTW.ESTIMATE,
-    timelimit :: Float64 = Inf,
-)
+immutable FourierLayer <: Mocha.Layer
+    name::AbstractString
+    bottoms::Vector{Symbol}
+    tops::Vector{Symbol}
+    pathkeys::Vector{PathKey}
+    flags::UInt32
+    timelimit::Float64
+end
+
+function FourierLayer(
+        name::AbstractString = "fourier",
+        bottoms::Vector{Symbol} = Symbol[],
+        tops::Vector{Symbol} = Symbol[],
+        pathkeys::Vector{PathKey} = PathKey[],
+        flags::UInt32 = FFTW.ESTIMATE,
+        timelimite::Float64 = Inf)
+    @assert length(bottoms) > 0
+    @assert length(bottoms) == length(tops)
+    FourierLayer(name, bottoms, tops, pathkeys, flags, timelimit)
+end
 
 Mocha.@characterize_layer(FourierLayer,
     has_neuron => false,
