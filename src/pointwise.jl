4b3ca524ecccfe6f3e4c019e73b011bc920f0bf5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-25 18:05:40 +0100

    include pointwiselayerstate.jl

--- src/pointwise.jl
+++ src/pointwise.jl
@@ -23,27 +23,6 @@ immutable PointwiseLayerState{BLOB<:ScatteredBlob,P<:AbstractPointwise}
     blobs_diff::Vector{BLOB}
 end
 
-function PointwiseLayerState{B<:ScatteredBlob}(
-        backend::Mocha.CPUBackend,
-        layer::PointwiseLayer,
-        inputs::Vector{B})
-    blobs = Vector{ScatteredBlob}(length(inputs))
-    for idblob in eachindex(inputs)
-        pairs = collect(inputs[idblob].nodes)
-        blobs[idblob] = ScatteredBlob(Dict(map(layer.ρ, pairs)))
-    end
-    return PointwiseLayerState(layer, blobs, ScatteredBlob[])
-end
-
-function forward{B<:ScatteredBlob}(
-        backend::Mocha.CPUBackend,
-        layerstate::PointwiseLayerState,
-        inputs::Vector{B})
-    for id in eachindex(inputs)
-        map!(layerstate.ρ, layerstate.blobs[id], inputs[id])
-    end
-end
-
 function Base.map(ρ::AbstractPointwise, blob_in::ScatteredBlob)
     blob_out = map(pair -> (pair.first => ρ(pair.second), blob_in))
 end
---
+++ src/pointwiselayerstate.jl
@@ -0,0 +1,20 @@
+function PointwiseLayerState{B<:ScatteredBlob}(
+        backend::Mocha.CPUBackend,
+        layer::PointwiseLayer,
+        inputs::Vector{B})
+    blobs = Vector{ScatteredBlob}(length(inputs))
+    for idblob in eachindex(inputs)
+        pairs = collect(inputs[idblob].nodes)
+        blobs[idblob] = ScatteredBlob(Dict(map(layer.ρ, pairs)))
+    end
+    return PointwiseLayerState(layer, blobs, ScatteredBlob[])
+end
+
+function forward{B<:ScatteredBlob}(
+        backend::Mocha.CPUBackend,
+        layerstate::PointwiseLayerState,
+        inputs::Vector{B})
+    for id in eachindex(inputs)
+        map!(layerstate.ρ, layerstate.blobs[id], inputs[id])
+    end
+end
