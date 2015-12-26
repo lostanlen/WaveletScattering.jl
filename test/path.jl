d5a92900ac71c99fcf66305d99b1174b2ca1533e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-26 14:22:08 +0100

    write PathKey tests

--- test/path.jl
+++ test/path.jl
@@ -5,3 +5,12 @@ import WaveletScattering: Literal, PathKey
 @test Literal(:time).depth == 1
 @test isimmutable(Literal((:γ, 2)))
 @test Literal((:γ, 2)).depth == 2
+
+# PathKey
+@test isimmutable(PathKey())
+@test isempty(PathKey().literals)
+@test isimmutable PathKey(:time)
+@test PathKey(:time).literals == [Literal(:time)]
+@test PathKey(PathKey(:time)) == PathKey(:time)
+@test isimmurabl(PathKey(:γ, 2, :time))
+@test PathKey(:γ, 2, :time) == [Literal(:γ,2), Literal(:time)]
