a473042a055688159720be79a41217750fc12df4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-02 15:03:20 +0100

    test SpatialDomain

--- test/domain.jl
+++ test/domain.jl
@@ -8,3 +8,11 @@ import WaveletScattering: AbstractDomain, FourierDomain, LineDomains,
 @test_throws AssertionError FourierDomain(0)
 @test_throws AssertionError FourierDomain(-1)
 @test_throws MethodError FourierDomain(0.5)
+
+# SpatialDomain
+@test issubtype(SpatialDomain, AbstractDomain)
+@test SpatialDomain(1).dim == Val{1}()
+@test SpatialDomain(2).dim == Val{2}()
+@test_throws AssertionError SpatialDomain(0)
+@test_throws AssertionError SpatialDomain(-1)
+@test_throws MethodError FourierDomain(0.5)
