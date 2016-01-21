3cb8c01094b25e8df4316ce344271e05289b607d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-21 15:28:31 +0100

    hooray, Heisenberg tests are passing

--- test/meta.jl
+++ test/meta.jl
@@ -1,6 +1,14 @@
 using Base.Test
 
-import WaveletScattering: default_ɛ
+# meta.jl
+import WaveletScattering: get_bandwidth, get_centerfrequency, get_qualityfactor,
+    get_scale
+
+# spec.jl
+import WaveletScattering: default_ɛ, uncertainty
+
+# spec1d.jl
+import WaveletScattering: Spec1D
 
 # bandwidths, centerfrequencies, qualityfactors, scales, uncertainty
 numerictypes = [Float16, Float32, Float64]
@@ -14,20 +22,19 @@ for T in numerictypes, nfo in nfos, max_q in nfos[nfos.<=nfo],
             max_qualityfactor=max_q,
             log2_size=log2_s,
             max_scale=max_s)
-    bws = bandwidths(spec)
-    ξs = centerfrequencies(spec)
-    qs = qualityfactors(spec)
-    scs = scales(spec)
+    bws = map(get_bandwidth, spec.ψmetas)
+    ξs = map(get_centerfrequency, spec.ψmetas)
+    qs = map(get_qualityfactor, spec.ψmetas)
+    scs = map(get_scale, spec.ψmetas)
     # bandwidths
     @test_approx_eq bws ξs./qs
     # centerfrequencies
     @test_approx_eq ξs[1] spec.motherfrequency
-    difflogξs = diff(log2(ξs))
+    difflogξs = diff(log2(ξs[:]))
     @test_approx_eq difflogξs (-ones(difflogξs)/spec.nFilters_per_octave)
     @test all(ξs.>0.0)
     @test_approx_eq ξs bws.*qs
     # qualityfactors
-    qs = qualityfactors(spec)
     @test all(qs.>=0.0)
     @test all(qs.<=max_q)
     @test_approx_eq qs ξs./bws
@@ -37,6 +44,6 @@ for T in numerictypes, nfo in nfos, max_q in nfos[nfos.<=nfo],
     @test all(scs .< (exp2(spec.log2_size[1])+machine_precision))
     # uncertainty
     empirical_uncertainty = bws .* scs
-    @test all(abs(diff(empirical_uncertainty)) .< machine_precision)
+    @test all(abs(diff(empirical_uncertainty[:])) .< machine_precision)
     @test all(abs(uncertainty(spec)-empirical_uncertainty).< machine_precision)
 end
