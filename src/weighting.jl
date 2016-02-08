413b1119b234a3e2f2081f507beeb7a47958637f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-08 17:26:01 +0100

    update weight_frequencies according to amplitude (not its square)

--- src/weighting.jl
+++ src/weighting.jl
@@ -10,11 +10,11 @@ end
 function weight_frequencies(weighting::LoudnessWeighting, ξs)
     freqs = weighting.samplerate * ξs
     freqs2 = freqs .* freqs
-    freqs4 = freqs2 .* freqs2
-    numerator = 12200.0 * 12200.0 * freqs
-    den1 = (freqs2 + 20.6 * 20.6)
-    den2 = sqrt((freqs2 + 107.7 * 107.7) * (freqs2 + 737.9 * 737.9))
-    den3 = (freqs2 + 12200.0 * 12200.0)
-    denominator = den1 .* den2 .* den3
+    numerator = 12200.0 * freqs2
+    sqden1 = (freqs2 + 20.6 * 20.6)
+    sqden2 = sqrt((freqs2 + 107.7 * 107.7) .* (freqs2 + 737.9 * 737.9))
+    sqden3 = (freqs2 + 12200.0 * 12200.0)
+    sqdenominator = sqden1 .* sqden2 .* sqden3
+    denominator = sqrt(sqdenominator)
     return numerator ./ denominator
 end
