c4196432fecef09f5189370cf4a7c2e67ceb09e0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-08 19:33:07 +0100

    early stopping in appendsymbols!

--- src/symbols.jl
+++ src/symbols.jl
@@ -1,3 +1,4 @@
 function appendsymbols!(symbols::Vector{Symbol}, N::Int)
-    symbols = vcat(symbols, [ symbol(:var, n) for n in 1:(length(symbols)-N) ])
+    length(symbols) == N && return
+    symbols = vcat(symbols, [ symbol(:var, n) for n in 1:(N-length(symbols)) ])
 end
