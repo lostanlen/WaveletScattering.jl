5b6a1ca03527eb58b8b0ee4ddb328c4d95106654
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-08 16:56:04 +0100

    typo in Behavior1D constructor

--- src/behavior1d.jl
+++ src/behavior1d.jl
@@ -70,7 +70,7 @@ function Behavior1D{
     ψ_log2_samplings = clamp(ψ_critical_log2_samplings + log2_oversampling,
         -max_log2_stride, 0)
     max_log2_stride = - min(ϕ_log2_sampling, minimum(ψ_log2_samplings))
-    ξs = [ get_centerfrequency(meta) for meta in ψmetas[1, :, :] ]
+    ξs = [ get_centerfrequency(meta) for meta in spec.ψmetas[1, :, :] ]
     weights = convert(T, weight_frequencies(weighting, ξs))
     Behavior1D(ϕ_log2_sampling, ψ_log2_samplings, is_ϕ_applied, j_range,
         log2_oversampling, max_log2_stride, pathkey, weighting, weights)
