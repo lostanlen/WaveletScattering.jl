1968391fb594f38a7efc5347556cb168802862f2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-08 17:10:46 +0100

    udpate behavior1d

--- src/behavior1d.jl
+++ src/behavior1d.jl
@@ -70,7 +70,7 @@ function Behavior1D{
     ψ_log2_samplings = clamp(ψ_critical_log2_samplings + log2_oversampling,
         -max_log2_stride, 0)
     max_log2_stride = - min(ϕ_log2_sampling, minimum(ψ_log2_samplings))
-    ξs = map(get_centerfrequency(meta))
+    ξs = map(get_centerfrequency, spec.ψmetas)
     weights = convert(Array{T, 3}, weight_frequencies(weighting, ξs))
     Behavior1D(ϕ_log2_sampling, ψ_log2_samplings, is_ϕ_applied, j_range,
         log2_oversampling, max_log2_stride, pathkey, weighting, weights)
