b167b57e349480878bc2d0ee64eb787365a2f9a1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-26 18:06:07 +0100

    update test/pointwise

---
+++ test/pointwise.jl
@@ -0,0 +1,13 @@
+using Base.Test
+# pointwise.jl
+import WaveletScattering: Identity, Log1P
+
+ρ = Identity()
+@test_approx_eq ρ([1.0, 2.0, 3.0]) [1.0, 2.0, 3.0]
+
+threshold = 0.1
+ρ = Log1P(threshold)
+@test_approx_eq ρ([0.0]) [0.0]
+@test_approx_eq expm1(ρ([1.0, 2.0, 3.0])) [1.0, 2.0, 3.0]*threshold
+@test Log1P(0.0)
+@test_throws DomainError Log1P(-realmin())
