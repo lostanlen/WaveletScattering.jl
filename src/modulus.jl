43e104d50c01566c55987f54672cf74f12baf564
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-01 10:52:05 +0200

    simplify src/modulus.jl

--- src/modulus.jl
+++ src/modulus.jl
@@ -1,17 +1,9 @@
 immutable Modulus <: AbstractPointwise
 end
 
-call{T,N}(ρ::Modulus, data::AbstractArray{T,N}) = abs(data)
+call(ρ::Modulus, data::AbstractArray) = abs(data)
 
 immutable SquaredModulus <: AbstractPointwise
 end
 
-call{T,N}(ρ::SquaredModulus, data::AbstractArray{T,N}) = abs2(data)
-
-
-function Base.map!{T<:Real}(
-        ρ::Modulus,
-        data_out::Array{T},
-        data_in::Array{Complex{T}})
-    map!(abs, data_out, data_in)
-end
+call(ρ::SquaredModulus, data::AbstractArray) = abs2(data)
