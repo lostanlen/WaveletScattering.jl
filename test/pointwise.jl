ab541fe0a5e45c06cb6986bb1a5f3aef52dc3861
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-01 22:33:12 +0200

    update test/pointwise.jl

--- test/pointwise.jl
+++ test/pointwise.jl
@@ -9,5 +9,4 @@ threshold = 0.1
 ρ = Log1P(threshold)
 @test_approx_eq ρ([0.0]) [0.0]
 @test_approx_eq expm1(ρ([1.0, 2.0, 3.0])) [1.0, 2.0, 3.0]*threshold
-@test Log1P(0.0)
-@test_throws DomainError Log1P(-realmin())
+@test_throws DomainError ρ([- 1.001 / threshold])
