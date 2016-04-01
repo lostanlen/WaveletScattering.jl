e8956c4a49b572e9a5c7fb2169e7407c5e07b099
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-01 12:30:57 +0200

    test behavior1d.jl

---
+++ test/behavior1d.jl
@@ -0,0 +1,35 @@
+using Base.Test
+# behavior1d.jl
+import WaveletScattering: Behavior1D
+# bank.jl
+import WaveletScattering: renormalize!
+# path.jl
+import WaveletScattering: PathKey
+# spec1d.jl
+import WaveletScattering: Spec1D
+# weighting.jl
+import WaveletScattering: EqualWeighting
+
+pathkey = PathKey(:time)
+spec = Spec1D()
+(nΘs, nΧs, nJs) = size(spec.ψmetas)
+domaintype = typeof(spec.domain)
+ψs = Array(AbstractFilter{spec.signaltype,domaintype}, (nΘs, nΧs, nJs))
+ψs[1, :, :] =
+    pmap(AbstractFilter, spec.ψmetas[1, :, :], fill(spec, nΧs * nJs))
+(nΘs > 1) && spin!(ψs)
+ϕ = AbstractFilter(spec.ϕmeta, spec)
+renormalize!(ϕ, ψs, spec)
+
+is_ϕ_applied = false
+j_range = 0:(spec.nOctaves-1)
+weighting = EqualWeighting()
+
+for log2_oversamping = 0:5
+    for max_log2_stride = 0:(spec.nOctaves-1)
+        behavior = Behavior1D(ϕ, ψs, spec, is_ϕ_applied, j_range,
+            log2_oversampling, max_log2_stride, pathkey, weighting)
+        @test all(behavior.ψ_log2_samplings .<= 0)
+        @test all(behavior.ψ_log2_samplings .>= -max_log2_stride)
+    end
+end
