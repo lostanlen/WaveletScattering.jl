c6df46ea530061a7332ac8f6484f822de3de583c
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-04 22:54:00 +0200

    typo in test/inputlayer.jl

--- test/inputlayer.jl
+++ test/inputlayer.jl
@@ -7,7 +7,7 @@ inputlayer = InputLayer(
         symbols = [:time, :chunk],
         data = rand(Float32, 256, 2))
 
-@test !Mocha.can_do_bp(signal)
-@test !Mocha.has_neuron(signal)
-@test !Mocha.has_param(signal)
-@test Mocha.is_source(signal)
+@test !Mocha.can_do_bp(inputlayer)
+@test !Mocha.has_neuron(inputlayer)
+@test !Mocha.has_param(inputlayer)
+@test Mocha.is_source(inputlayer)
