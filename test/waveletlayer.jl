35d5fc6bf3fa36efc5f28587c7e0e5926cfb7065
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-04 22:06:32 +0200

    test AssertionError's in WaveletLayer constructor

--- test/waveletlayer.jl
+++ test/waveletlayer.jl
@@ -2,7 +2,7 @@ using Base.Test
 # waveletlayer.jl
 import WaveletScattering: WaveletLayer
 # bank1d.jl
-import WaveletScattering: Bank1D
+import WaveletScattering: NullBank, Bank1D
 # spec1d.jl
 import WaveletScattering: Spec1D
 
@@ -10,8 +10,17 @@ bank = Bank1D(Spec1D())
 bottoms = [:fourier]
 name = "wavelets"
 tops = [:wavelets]
-waveletlayer = WaveletLayer(bank, bottoms, name, tops)
-
+waveletlayer = WaveletLayer(bank=bank,
+    bottoms=bottoms, name=name, tops=tops)
 @test Mocha.can_do_bp(waveletlayer)
 @test !Mocha.has_neuron(waveletlayer)
 @test !Mocha.has_param(waveletlayer)
+
+@test_throws AssertionError WaveletLayer(bank=NullBank(),
+    bottoms=bottoms, name=name, tops=tops)
+@test_throws AssertionError WaveletLayer(bank=bank,
+    bottoms=Symbol[], name=name, tops=tops)
+@test_throws AssertionError WaveletLayer(bank=bank,
+    bottoms=bottoms, name=name, tops=[:top1, :top2])
+@test_throws AssertionError WaveletLayer(bank=bank,
+    bottoms=[:bottom1, :bottom2], name=name, tops=tops)
