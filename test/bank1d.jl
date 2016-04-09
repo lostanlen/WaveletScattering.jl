4d2438c671ca290d912042af9e61d1aa99e904a4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-09 22:17:43 +0200

    update test/bank1d.jl

--- test/bank1d.jl
+++ test/bank1d.jl
@@ -4,5 +4,8 @@ import WaveletScattering: Bank1D
 # spec1d.jl
 import WaveletScattering: Spec1D
 
-bank = Bank1D(Spec1D())
-@test ndims(bank) == 1
+W = Bank1D(Spec1D())
+@test ndims(W) == 1
+
+x = map(Float32, randn(1 << W.spec.log2_size[1]))
+@test W(x)
