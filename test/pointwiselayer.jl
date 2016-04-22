4fb83fb296380eb1501a303888802e3ed54d5ce6
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-22 12:53:15 +0200

    bis

--- test/pointwiselayer.jl
+++ test/pointwiselayer.jl
@@ -3,6 +3,6 @@ import WaveletScattering: PointwiseLayer
 
 modulus = PointwiseLayer(bottoms = [:bottom], tops = [:top])
 
-@test Mocha.can_do_bp(modulus)
+@test !Mocha.can_do_bp(modulus)
 @test !Mocha.has_neuron(modulus)
 @test !Mocha.has_param(modulus)
