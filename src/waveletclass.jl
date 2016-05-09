52dabad2d0044f05946b5a6527f76d8aab50e783
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-05-09 18:42:41 +0200

    define Gammatone and MexicanHat types

--- src/waveletclass.jl
+++ src/waveletclass.jl
@@ -1,3 +1,7 @@
 abstract RedundantWaveletClass
 
+immutable Gammatone <: RedundantWaveletClass end
+
+immutable MexicanHat <: RedundantWaveletClass end
+
 immutable Morlet <: RedundantWaveletClass end
