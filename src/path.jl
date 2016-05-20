72954b3252e9bcb3f6f586ca899b85899c0b69e9
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-05-20 10:22:16 +0200

    whitespace in src/path.jl

--- src/path.jl
+++ src/path.jl
@@ -122,5 +122,4 @@ immutable PathRange
     end
 end
 
-Base.(:(==))(x::PathRange, y::PathRange) =
-    reduce(&, map(==, x.sdict, y.sdict))
+Base.(:(==))(x::PathRange, y::PathRange) = reduce(&, map(==, x.sdict, y.sdict))
