f5b9e2ea0497c77d9e9d2aa727a677a2b23bc708
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-05-21 15:44:21 +0200

    update test/bank1d.jl

--- test/bank1d.jl
+++ test/bank1d.jl
@@ -1,13 +1,21 @@
 using Base.Test
 # bank1d.jl
 import WaveletScattering: Bank1D
+# blob.jl
+import WaveletScattering: ScatteredBlob
+# node.jl
+import WaveletScattering: InvComplexFourierNode
+# path.jl
+import WaveletScattering: Path
 # spec1d.jl
 import WaveletScattering: Spec1D
 
-W = Bank1D(Spec1D())
+W = Bank1D(Spec1D(nFilters_per_octave = 4))
 @test ndims(W) == 1
 
-x = map(Float32, randn(1 << W.spec.log2_size))
+x = zeros(Float32, 1 << W.spec.log2_size)
+x[1] = 1.0
 Wx = W(x)
-@test isa(Wx,
-    WaveletScattering.ScatteredBlob{WaveletScattering.Node{Complex{Float32},2}})
+
+
+@test isa(Wx, ScatteredBlob{InvComplexFourierNode{Complex{Float32},2,Float32}})
