661a59ad975565a806c7af39c552628a6d1d5454
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-06-10 17:56:23 +0200

    write Base.abs for ScatteredBlob

--- src/pointwise.jl
+++ src/pointwise.jl
@@ -12,6 +12,21 @@ immutable Log1P{T<:AbstractFloat} <: AbstractPointwise
     end
 end
 
+function Base.abs{NODE<:AbstractNode}(nodes::DataStructures.SortedDict{
+        WaveletScattering.Path,NODE,Base.Order.ForwardOrdering})
+    nodevalues = collect(values(nodes))
+    isempty(nodevalues) && return nodes
+    T = real(eltype(nodevalues[1].data))
+    dim = ndims(nodevalues[1].data)
+    absnodes =
+        DataStructures.SortedDict{Path,Node{T,dim},Base.Order.ForwardOrdering}
+    for (path, nodevalue) in nodes
+        absnodes[path] = Node(abs(nodevalue.data), nodevalue.ranges)
+    end
+end
+
+Base.abs(Wx::ScatteredBlob) = ScatteredBlob(abs(Wx.nodes))
+
 function Base.call(ρ::AbstractPointwise, blob_in::ScatteredBlob)
     blob_out = ScatteredBlob(map(ρ, blob_in.nodes))
 end
