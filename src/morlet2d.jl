a2f63aae8d35c1770e3ef10b5e897291513e2b65
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-06-12 19:21:15 +0200

    update src/morlet2d.jl

--- src/morlet2d.jl
+++ src/morlet2d.jl
@@ -15,17 +15,33 @@ function AbstractFilter{T<:FFTW.fftwReal,G<:LineGroups}(ψmeta::ΨMeta,
     bw = N * T(ψmeta.bandwidth)
     den = @fastmath bw * bw / T(2.0 * log(2.0))
     """2. **Angle**"""
-    nOrientations = get_nOrientations(spec)
-    angle = 2π * ψmeta.θ / nOrientations
-
+    nOrientations = get_nOrientations(spec.pointgroup)
+    θ = T(2π * ψmeta.θ / nOrientations)
     """3. **Number of periods**"""
     halfsupport = sqrt(den * log(inv(spec.ɛ)))
     firstω = max(center - halfsupport, -5N/2)
     lastω = min(center + halfsupport, 5N/2 - 1)
-    nPeriods = 1 + ceil(Int, (lastω-halfN) / N
+    nPeriods = 1 + ceil(Int, (lastω-halfN) / N)
     #"""3. **Call to morlet**"""
-    aspectratio = T(ψ.aspectratio)
-    #y = morlet(FourierDomain(1), angle, aspectratio, center, den, N, nPeriods)
+    aspectratio = T(ψmeta.aspectratio)
+    y = morlet(FourierDomain(1), θ, aspectratio, center, den, N, nPeriods)
     #"""4. **Trimming to true support boundaries**"""
     #return AbstractFilter(y, spec)
+end
+
+function morlet{T<:FFTW.fftwReal}(::FourierDomain{2},
+        θ::T, aspectratio::T, center::T, den::T, N::Int, nPeriods::Int)
+    halfN = N >> 1
+    pstart = - ((nPeriods-1)>>1)
+    pstop = (nPeriods-1)>>1 + iseven(nPeriods)
+    ωstart = - halfN + pstart * N
+    ωstop = halfN + pstop * N - 1
+    ωrange = ωstart:ωstop
+    nΩs = length(ωrange)
+    ωrows = repmat(-ωrange, 1, nΩs)
+    ωcols = repmat(ωrange', nΩs, 1)
+    cosθ = cos(θ)
+    sinθ = sin(θ)
+    ωrows_θ = ωrows * cosθ + ωcols * sinθ
+    ωcols_θ = -ωrows * sinθ + ωrows * cosθ
 end
