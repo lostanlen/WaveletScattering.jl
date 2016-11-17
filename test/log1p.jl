4b999b100afd72ee86c7baec577aaa14cbc1c362
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-11-17 16:38:39 +0100

    expm1.

--- test/log1p.jl
+++ test/log1p.jl
@@ -2,7 +2,7 @@ using Base.Test
 import WaveletScattering: Log1P
 
 x = [0.0, 1.0, 2.0, 3.0]
-data_in = expm1(x)
+data_in = expm1.(x)
 data_out = zeros(data_in)
 ρ = Log1P(1.0)
 Base.map!(ρ, data_out, data_in)
