8a2feebbe412057b438d45f817fbc8cf8f8cb4be
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-11-17 16:12:00 +0100

    abs. abs2.

--- src/modulus.jl
+++ src/modulus.jl
@@ -1,14 +1,14 @@
 immutable Modulus <: AbstractPointwise
 end
 
-(ρ::Modulus)(data::AbstractArray) = abs(data)
+(ρ::Modulus)(data::AbstractArray) = abs.(data)
 
 (ρ::Modulus)(blob_in::ScatteredBlob) = map(ρ, ScatteredBlob(blob_in.nodes))
 
 immutable SquaredModulus <: AbstractPointwise
 end
 
-(ρ::SquaredModulus)(data::AbstractArray) = abs2(data)
+(ρ::SquaredModulus)(data::AbstractArray) = abs2.(data)
 
 (ρ::SquaredModulus)(blob_in::ScatteredBlob) =
     map(ρ, ScatteredBlob(blob_in.nodes))
