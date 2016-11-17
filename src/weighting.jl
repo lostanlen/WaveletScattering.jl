ef861b85ae9d03250b9f1ea56c57ac187c55b647
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-11-17 15:40:34 +0100

    sqrt.

--- src/weighting.jl
+++ src/weighting.jl
@@ -12,9 +12,9 @@ function weight_frequencies(weighting::LoudnessWeighting, ξs)
     freqs2 = freqs .* freqs
     numerator = 12200.0 * freqs2
     sqden1 = (freqs2 + 20.6 * 20.6)
-    sqden2 = sqrt((freqs2 + 107.7 * 107.7) .* (freqs2 + 737.9 * 737.9))
+    sqden2 = sqrt.((freqs2 + 107.7 * 107.7) .* (freqs2 + 737.9 * 737.9))
     sqden3 = (freqs2 + 12200.0 * 12200.0)
     sqdenominator = sqden1 .* sqden2 .* sqden3
-    denominator = sqrt(sqdenominator)
+    denominator = sqrt.(sqdenominator)
     return numerator ./ denominator
 end
