458a56591723fbab3c0d06d3d7c17aec3fb7a5c1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-12-13 18:03:54 +0100

    bis

--- src/modulus.jl
+++ src/modulus.jl
@@ -11,4 +11,4 @@ end
 (ρ::SquaredModulus)(data::AbstractArray) = abs2.(data)
 
 (ρ::SquaredModulus)(blob_in::ScatteredBlob) =
-    ScatteredBlob(map(ρ, blob_in.nodes)
+    ScatteredBlob(map(ρ, blob_in.nodes))
