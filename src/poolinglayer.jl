e3da23d76b6f258831bef826daf7855ac3be367a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2017-01-07 22:21:24 -0500

    write poolinglayer.jl

---
+++ src/poolinglayer.jl
@@ -0,0 +1,22 @@
+immutable PoolingLayer <: Mocha.Layer
+    bottoms::Vector{Symbol}
+    name::AbstractString
+    pathkeys::Vector{PathKey}
+    pooling::Pooling
+    tops::Vector{Symbol}
+end
+
+function PoolingLayer( ;
+        bottoms::Vector{Symbol} = Symbol[],
+        name::AbstractString = "pointwise",
+        pathkeys::Vector{PathKey} = PathKey[],
+        pooling::Function = mean,
+        tops::Vector{Symbol} = Symbol[])
+    @assert length(bottoms) > 0
+    @assert length(tops) == length(bottoms)
+    PoolingLayer(bottoms, name, pathkeys, pooling, tops)
+end
+
+Mocha.can_do_bp(::PoolingLayer) = false
+Mocha.has_neuron(::PoolingLayer) = false
+Mocha.has_param(::PoolingLayer) = false
