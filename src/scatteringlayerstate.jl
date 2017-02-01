6bf16cfe3978e1cae3ac1d9ff5774930b6d51c78
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2017-01-31 20:11:28 -0500

    scatteredlayerstate -> scatteringlayerstate

--- src/scatteringlayerstate.jl
+++ src/scatteringlayerstate.jl
@@ -1,5 +1,13 @@
-# ScatteredLayerState
-immutable ScatteredLayerState{B<:ScatteredBlob} <: AbstractScatteredLayerState
+# ScatteringLayerState
+immutable ScatteringLayerState{B<:ScatteredBlob} <: AbstractScatteredLayerState
     blobs::Vector{B}
     layer::ScatteringLayer
+end
+
+function Mocha.setup(
+        backend::Mocha.CPUBackend,
+        layer::ScatteringLayer,
+        inputs::Vector{Mocha.Blob},
+        diffs::Vector{Mocha.Blob})
+    return ScatteringLayerState(backend, layer, inputs)
 end
