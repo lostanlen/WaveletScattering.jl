557a14aaa8e7118113aefe732ea28b3563580d7e
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-02-16 16:48:12 -0500

    Update ScatteringLayer

--- src/scatteringlayer.jl
+++ src/scatteringlayer.jl
@@ -2,6 +2,7 @@ immutable ScatteringLayer <: Mocha.Layer
     bank::AbstractBank
     bottoms::Vector{Symbol}
     name::AbstractString
+    previous_bank::AbstractBank
     sibling_mask_factor::Float64
     tops::Vector{Symbol}
 end
@@ -14,9 +15,11 @@ function ScatteringLayer( ;
         bank::AbstractBank = NullBank(),
         bottoms::Vector{Symbol} = Symbol[],
         name::AbstractString = "scattering",
+        previous_bank::AbstractBank = NullBank(),
         sibling_mask_factor = 1.0,
         tops::Vector{Symbol} = Symbol[])
-    @assert length(bottoms) == 2
+    @assert length(bottoms) == 1
     @assert length(tops) == 1
-    ScatteringLayer(bank, bottoms, name, sibling_mask_factor, tops)
+    ScatteringLayer(bank, bottoms, name, previous_bank, sibling_mask_factor,
+        tops)
 end
