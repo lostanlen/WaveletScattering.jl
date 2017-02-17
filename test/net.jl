d3d1ecf63c72894ee489576d5de47aae06c093fa
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-02-16 21:49:18 -0500

    We have a scattering transform \o/

--- test/net.jl
+++ test/net.jl
@@ -26,29 +26,29 @@ import WaveletScattering: ScatteringLayer
 J = 8
 data = zeros(Float32, 2^J, 2)
 data[1] = 1.0f0
+bank1 = Bank1D(Spec1D(log2_size=J, n_filters_per_octave=1))
+bank2 = Bank1D(Spec1D(log2_size=J, n_filters_per_octave=1))
 
 backend = Mocha.CPUBackend()
 signal = InputLayer(
+    data = data,
     tops = [:signal],
-    symbols = [:time, :chunk],
-    data = data)
+    symbols = [:time, :chunk])
 
 fourier = FourierLayer(
     bottoms = [:signal],
-    tops = [:fourier],
-    pathkeys = [PathKey(:time)])
+    pathkeys = [PathKey(:time)],
+    tops = [:fourier])
 
 wavelets = WaveletLayer(
-    bank = Bank1D(Spec1D(log2_size=J, n_filters_per_octave=4)),
+    bank = bank1,
     bottoms = [:fourier],
-    tops = [:wavelets]
-)
+    tops = [:wavelets])
 
 invfourier = InvFourierLayer(
     bottoms = [:wavelets],
-    tops = [:invfourier],
-    pathkeys = [PathKey(:time)]
-)
+    pathkeys = [PathKey(:time)],
+    tops = [:invfourier])
 
 modulus = PointwiseLayer(
     bottoms = [:invfourier],
@@ -58,16 +58,16 @@ modulus = PointwiseLayer(
 fourier2 = FourierLayer(
     bottoms = [:modulus],
     name = "fourier2",
-    tops = [:fourier2],
-    pathkeys = [PathKey(:time)]
-)
+    pathkeys = [PathKey(:time)],
+    tops = [:fourier2])
 
 scattering = ScatteringLayer(
-    bank = Bank1D(Spec1D(log2_size=J, n_filters_per_octave=2)),
-    bottoms = [:fourier2, :wavelets],
+    bank = bank2,
+    bottoms = [:fourier2],
     name = "scattering",
-    tops = [:scattering]
-)
+    sibling_mask_factor = 1.0,
+    previous_bank = bank2,
+    tops = [:scattering])
 
 layers = Mocha.Layer[
     signal,
@@ -82,21 +82,78 @@ net = Mocha.Net("network", backend, layers)
 
 @test isa(net, Mocha.Net{Mocha.CPUBackend})
 
-import WaveletScattering: get_bandwidth, get_centerfrequency
+import WaveletScattering: AbtractNode, get_bandwidth, get_centerfrequency,
+    prepend, scattering_order
 
 # setup
 layer = scattering
-inputs = [fourier2, wavelets]
-previous_ψmetas = inputs[2].bank.spec.ψmetas
+inputs = net.states[6].blobs
+
+innodes = inputs[1].nodes
+
+firstpath = collect(keys(innodes))[1]
+pathkey = layer.bank.behavior.pathkey
+order = scattering_order(firstpath, pathkey)
+assert(order > 0)
+
+j1key = prepend((:j, order), pathkey)
+
+previous_bank = layer.previous_bank
+previous_ψmetas = previous_bank.spec.ψmetas
 previous_bandwidths = map(get_bandwidth, previous_ψmetas)
+previous_log2_samplings = previous_bank.behavior.ψ_log2_samplings
+
 ψmetas = layer.bank.spec.ψmetas
 centerfrequencies = map(get_centerfrequency, ψmetas)
 n_octaves = size(centerfrequencies, 3)
-last_j1s = Array{Int}(n_octaves)
+first_j1 = layer.previous_bank.behavior.j_range.start
 
-for j2 in 0:(n_octaves-1)
+paths = collect(keys(innodes))
+
+chromakey = prepend(:χ, layer.bank.behavior.pathkey)
+chromarange = chromakey => 0:1:(layer.bank.spec.n_filters_per_octave-1)
+
+outnodes = DataStructures.SortedDict{Path,AbstractNode,
+    Base.Order.ForwardOrdering}()
+
+for j2 in layer.bank.behavior.j_range
     centerfrequency = centerfrequencies[1, end, 1+j2]
-    last_j1s[1+j2] = findlast( sibling_mask_factor *
-        previous_bandwidths[1, 1, :] .>= centerfrequency) - 1
-    
+    bandwidths = layer.sibling_mask_factor * previous_bandwidths[1, 1, :]
+    last_j1 = findlast(bandwidths .>= centerfrequency) - 1
+    last_j1 = min(last_j1, previous_bank.behavior.j_range.stop)
+    log2_sampling = layer.bank.behavior.ψ_log2_samplings[1+j2]
+    for inpath in keys(innodes)
+        innode = innodes[inpath]
+        j1 = inpath.sdict[j1key]
+        (j1 < first_j1) && continue
+        (j1 > last_j1) && continue
+        println(j1, j2)
+        inranges = innode.ranges
+        inpathkeys = [ pair.first for pair in collect(inranges) ]
+        subscripts = find(pathkey .== inpathkeys)
+        insizes = collect(size(innode.data))
+        if isa(innode, RealFourierNode)
+            insizes[subscripts] = 2 * (insizes[subscripts] - 1)
+        end
+        outranges = [inranges..., chromarange]
+        outsizes = [insizes ; layer.bank.spec.n_filters_per_octave]
+        previous_log2_sampling = previous_log2_samplings[1+j1]
+        log2_resampling = log2_sampling - previous_log2_sampling
+        for subscript in subscripts
+            outsizes[subscript] = insizes[subscript] >> (-log2_resampling)
+            inrange = inranges[subscript].second
+            outranges[subscript] = outranges[subscript].first =>
+                (inrange.start):(1<<-log2_resampling):(inrange.stop)
+        end
+        outdata = zeros(eltype(innode.data), tuple(outsizes...))
+        inds = [fill(Colon(), ndims(innode.data)) ; 0]
+        for χ in 0:(layer.bank.spec.n_filters_per_octave-1)
+            ψ = layer.bank.ψs[1, 1+χ, 1+j2]
+            println(typeof(ψ))
+            inds[end] = 1 + χ
+            transform!(view(outdata, inds...), ψ, innode, subscripts...)
+        end
+        outpath = Path(inpath.sdict..., octavekey => j2)
+        outnodes[outpath] = Node(outdata, tuple(outranges...))
+    end
 end
